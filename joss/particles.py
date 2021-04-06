a0ac530baef673fa53ec2f5178c2505cb1aae17d
Jan Kaiser <jan.kaiser@desy.de>
2021-04-06 14:38:01 +0200

    Fixed up particles generation (using Ocelot)

--- joss/particles.py
+++ joss/particles.py
@@ -1,22 +1,20 @@
 import numpy as np
+import ocelot as oc
 
 
 def generate_particles(n=100000, x=0, y=0, px=0, py=0, sigma_x=175e-9, sigma_y=175e-9,
                        sigma_px=2e-7, sigma_py=2e-7, sigma_s=0, sigma_p=0):
     """Generate n particles according to a multivariate normal distribution."""
-    mean = [x, y, px, py, 0, 0]
-    rx = 0
-    ry = 0
-    cov = [[sigma_x**2,          rx,          0,           0,          0,          0],
-           [        rx, sigma_px**2,          0,           0,          0,          0],
-           [         0,           0, sigma_y**2,          ry,          0,          0],
-           [         0,           0,         ry, sigma_py**2,          0,          0],
-           [         0,           0,          0,           0, sigma_s**2,          0],
-           [         0,           0,          0,           0,          0, sigma_p**2]]
-
-    particles = np.ones((n, 7))
-    particles[:,:6] = np.random.multivariate_normal(mean=mean, cov=cov, size=n)
-
+    particles = np.ones((int(1e+5), 7))
+    particles[:,:6] = oc.generate_parray(nparticles=n,
+                                         sigma_x=sigma_x,
+                                         sigma_px=sigma_px,
+                                         sigma_y=sigma_y,
+                                         sigma_py=sigma_py,
+                                         sigma_p=sigma_p,
+                                         chirp=0,
+                                         energy=0.1,
+                                         sigma_tau=0.0).rparticles.transpose()
     return particles
 
 
