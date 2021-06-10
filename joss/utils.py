f74db4157da343d85292e838b7ece135d93c561d
Jan Kaiser <jan.kaiser@desy.de>
2021-06-10 12:23:52 +0200

    More pythonic class checks

--- joss/utils.py
+++ joss/utils.py
@@ -1,3 +1,4 @@
+from numpy.lib.arraysetops import isin
 import ocelot as oc
 
 from joss import accelerator
@@ -22,22 +23,22 @@ def ocelot2joss(element):
     Object not supported by JOSS are translated to drift sections. Screen objects are created only
     from `ocelot.Monitor` objects when the string "SCR" in their `id` attribute.
     """
-    if element.__class__ is oc.Drift:
+    if isinstance(element, oc.Drift):
         return accelerator.Drift(element.l, name=element.id)
-    elif element.__class__ is oc.Quadrupole:
+    elif isinstance(element, oc.Quadrupole):
         return accelerator.Quadrupole(element.l, element.k1, name=element.id)
-    elif element.__class__ is oc.Hcor:
+    elif isinstance(element, oc.Hcor):
         return accelerator.HorizontalCorrector(element.l, element.angle, name=element.id)
-    elif element.__class__ is oc.Vcor:
+    elif isinstance(element, oc.Vcor):
         return accelerator.VerticalCorrector(element.l, element.angle, name=element.id)
-    elif element.__class__ is oc.Cavity:
+    elif isinstance(element, oc.Cavity):
         return accelerator.Cavity(element.l, name=element.id)
-    elif element.__class__ is oc.Monitor and "SCR" in element.id:
+    elif isinstance(element, oc.Monitor) and "SCR" in element.id:
         print("WARNING: A diagnostic screen was converted from Ocelot and its resolution and pixel size were set to default values. You may need to adjust these!")
         return accelerator.Screen((2448,2040), (3.5488e-6,2.5003e-6), name=element.id)
-    elif element.__class__ is oc.Monitor and "BPM" in element.id:
+    elif isinstance(element, oc.Monitor) and "BPM" in element.id:
         return accelerator.BPM(name=element.id)
-    elif element.__class__ is oc.Undulator:
+    elif isinstance(element, oc.Undulator):
         return accelerator.Undulator(element.l, name=element.id)
     else:
         return accelerator.Drift(element.l, name=element.id)
