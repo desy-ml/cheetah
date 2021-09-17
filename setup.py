6c2540567b66952c18e12dfb0c069039758767e3
Jan Kaiser <jan.kaiser@desy.de>
2021-09-17 15:53:29 +0200

    Updated to v0.5

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup
 
-setup(name="cheetah",
-      version="0.4.5",
-      author="Jan Kaiser & Oliver Stein",
-      author_email="jan.kaiser@desy.de | oliver.stein@desy.de",
-      description="Fast particle accelerator optics simulation for reinforcement learning and optimisation applications.",
-      url="https://github.com/desy-ml/cheetah",
-      install_requires=["torch>=1.9",
-                        "ocelot>=20.11.2",
-                        "scipy>=1.6.0"])
+
+setup(
+    name="cheetah",
+    version="0.5",
+    author="Jan Kaiser & Oliver Stein",
+    author_email="jan.kaiser@desy.de | oliver.stein@desy.de",
+    description="Fast particle accelerator optics simulation for reinforcement learning and optimisation applications.",
+    url="https://github.com/desy-ml/cheetah",
+    install_requires=[
+        "torch>=1.9",
+        "ocelot>=20.11.2",
+        "scipy>=1.6.0"
+    ]
+)
