17f8023e49bf6b04433cc7104301d026161395da
Jan Kaiser <jan.kaiser.email@googlemail.com>
2022-09-28 21:48:47 +0200

    Prepare PyPi release

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
+from pathlib import Path
+
 from setuptools import setup
 
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
-    name="cheetah",
-    version="0.5.13",
+    name="cheetah-accelerator",
+    version="0.5.14",
     author="Jan Kaiser & Oliver Stein",
     author_email="jan.kaiser@desy.de",
-    description="Fast particle accelerator optics simulation for reinforcement learning and optimisation applications.",
     url="https://github.com/desy-ml/cheetah",
+    description="Fast particle accelerator optics simulation for reinforcement learning and optimisation applications.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=["cheetah"],
     install_requires=[
         "torch",
