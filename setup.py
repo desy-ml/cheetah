4e23e0c77fc798cf3e20be5b5b8247075f129c88
Jan Kaiser <jan.kaiser.email@googlemail.com>
2023-02-02 12:53:35 +0100

    Update version

--- setup.py
+++ setup.py
@@ -7,11 +7,14 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="cheetah-accelerator",
-    version="0.5.15",
+    version="0.5.16",
     author="Jan Kaiser & Oliver Stein",
     author_email="jan.kaiser@desy.de",
     url="https://github.com/desy-ml/cheetah",
-    description="Fast particle accelerator optics simulation for reinforcement learning and optimisation applications.",
+    description=(
+        "Fast particle accelerator optics simulation for reinforcement learning and"
+        " optimisation applications."
+    ),
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=["cheetah"],
