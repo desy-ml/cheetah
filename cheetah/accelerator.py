994a5d313afd5686edbefd769ff28b79949a5d74
FelixTheilen <126663525+FelixTheilen@users.noreply.github.com>
2023-03-28 11:20:25 +0200

    DeviceError
    Creates an exception DeviceError. The exception is called, in case the device for calculating the segment differs from the device used to calculate the Beam.

--- cheetah/accelerator.py
+++ cheetah/accelerator.py
@@ -17,6 +17,13 @@ REST_ENERGY = (
     / constants.elementary_charge
 )
 
+class DeviceError(Exception):
+    """
+    Used to create an exception, in case the device used for the beam and the elements are different.
+    """
+    def __init__(self):
+        super().__init__("Warning! The device used for calculating the elements is not the same, as the device used to calculate the Beam.")
+
 
 class Element:
     """
@@ -80,10 +87,14 @@ class Element:
             tm = self.transfer_map(incoming.energy)
             mu = torch.matmul(tm, incoming._mu)
             cov = torch.matmul(tm, torch.matmul(incoming._cov, tm.t()))
+            if self.device != "cpu":
+                raise DeviceError
             return ParameterBeam(mu, cov, incoming.energy)
         elif isinstance(incoming, ParticleBeam):
             tm = self.transfer_map(incoming.energy)
             new_particles = torch.matmul(incoming.particles, tm.t())
+            if self.device != incoming.device:
+                raise DeviceError
             return ParticleBeam(new_particles, incoming.energy, device=incoming.device)
         else:
             raise TypeError(f"Parameter incoming is of invalid type {type(incoming)}")
