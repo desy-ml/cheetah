c3c97e75c4c72edbaeb9c0b3b49ce3d855adc923
Felix Theilen <felix.theilen@tuhh.de>
2023-04-03 12:40:38 +0200

    Update accelerator.py

--- cheetah/accelerator.py
+++ cheetah/accelerator.py
@@ -16,13 +16,6 @@ REST_ENERGY = (
     / constants.elementary_charge
 )
 
-class DeviceError(Exception):
-    """
-    Used to create an exception, in case the device used for the beam and the elements are different.
-    """
-    def __init__(self):
-        super().__init__("Warning! The device used for calculating the elements is not the same, as the device used to calculate the Beam.")
-
 
 class DeviceError(Exception):
     """
