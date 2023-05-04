9382bcd6074cc05634eb3e7e186efeff6d404810
Felix Theilen <felix.theilen@tuhh.de>
2023-05-04 12:47:42 +0200

    flake8 linting applied

--- cheetah/accelerator.py
+++ cheetah/accelerator.py
@@ -19,12 +19,14 @@ REST_ENERGY = (
 
 class DeviceError(Exception):
     """
-    Used to create an exception, in case the device used for the beam and the elements are different.
+    Used to create an exception, in case the device used for the beam
+    and the elements are different.
     """
 
     def __init__(self):
         super().__init__(
-            "Warning! The device used for calculating the elements is not the same, as the device used to calculate the Beam."
+            "Warning! The device used for calculating the elements is not the same, "
+            "as the device used to calculate the Beam."
         )
 
 
