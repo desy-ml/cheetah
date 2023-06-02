e4859e41cc8518ad1b8665c900df9f500d0aed8c
Jan Kaiser <jan.kaiser.email@googlemail.com>
2023-06-02 09:29:20 +0200

    Update authors

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (this_directory / "README.md").read_text()
 setup(
     name="cheetah-accelerator",
     version="0.5.19",
-    author="Jan Kaiser & Oliver Stein",
+    author="Jan Kaiser, Chenran Xu & Oliver Stein",
     author_email="jan.kaiser@desy.de",
     url="https://github.com/desy-ml/cheetah",
     description=(
