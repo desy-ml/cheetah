529786abc3a08f80736e39eef4f448dc9b30cdd7
Jan Kaiser <jan.kaiser.email@googlemail.com>
2023-09-29 09:18:39 +0200

    Fix error in the merged transfer map computation

--- cheetah/accelerator.py
+++ cheetah/accelerator.py
@@ -194,7 +194,7 @@ class CustomTransferMap(Element):
 
         tm = torch.eye(7, device=incoming_beam.device)
         for element in elements:
-            tm = torch.matmul(tm, element.transfer_map(incoming_beam.energy))
+            tm = torch.matmul(element.transfer_map(incoming_beam.energy), tm)
             incoming_beam = element.track(incoming_beam)
 
         combined_length = sum(element.length for element in elements)
