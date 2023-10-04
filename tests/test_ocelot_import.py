dacf2c473d908f17b44ce8800883e6c7ae77738c
Chenran Xu <chenran.xu@kit.edu>
2023-10-04 09:54:51 +0200

    Fix the charge conversion from ocelot; Add test functions for from_ocelot methods;

--- tests/test_ocelot_import.py
+++ tests/test_ocelot_import.py
@@ -1,6 +1,8 @@
+import numpy as np
+import ocelot
 import pytest
 
-from cheetah import Screen, Segment
+from cheetah import ParameterBeam, ParticleBeam, Screen, Segment
 
 from .resources import ARESlatticeStage3v1_9 as ares
 
@@ -31,3 +33,35 @@ def test_screen_conversion(name: str):
     segment = Segment.from_ocelot(ares.cell)
     screen = getattr(segment, name)
     assert isinstance(screen, Screen)
+
+
+def test_ocelot_to_parameterbeam():
+    parray = ocelot.astraBeam2particleArray("benchmark/astra/ACHIP_EA1_2021.1351.001")
+    beam = ParameterBeam.from_ocelot(parray)
+
+    assert np.allclose(beam.mu_x, np.mean(parray.x()))
+    assert np.allclose(beam.mu_xp, np.mean(parray.px()))
+    assert np.allclose(beam.mu_y, np.mean(parray.y()))
+    assert np.allclose(beam.mu_yp, np.mean(parray.py()))
+    assert np.allclose(beam.sigma_x, np.std(parray.x()))
+    assert np.allclose(beam.sigma_xp, np.std(parray.px()))
+    assert np.allclose(beam.sigma_y, np.std(parray.y()))
+    assert np.allclose(beam.sigma_yp, np.std(parray.py()))
+    assert np.allclose(beam.sigma_s, np.std(parray.tau()))
+    assert np.allclose(beam.sigma_p, np.std(parray.p()))
+    assert np.allclose(beam.energy, parray.E * 1e9)
+    assert np.allclose(beam.total_charge, parray.total_charge)
+
+
+def test_ocelot_to_particlebeam():
+    parray = ocelot.astraBeam2particleArray("benchmark/astra/ACHIP_EA1_2021.1351.001")
+    beam = ParticleBeam.from_ocelot(parray)
+
+    assert np.allclose(beam.particles[:, 0], parray.x())
+    assert np.allclose(beam.particles[:, 1], parray.px())
+    assert np.allclose(beam.particles[:, 2], parray.y())
+    assert np.allclose(beam.particles[:, 3], parray.py())
+    assert np.allclose(beam.particles[:, 4], parray.tau())
+    assert np.allclose(beam.particles[:, 5], parray.p())
+    assert np.allclose(beam.energy, parray.E * 1e9)
+    assert np.allclose(beam.particle_charges, parray.q_array)
