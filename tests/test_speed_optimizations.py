a94ff3b418db42e154399eca628e8cac33870f2c
Jan Kaiser <jan.kaiser.email@googlemail.com>
2024-02-14 15:46:12 +0100

    Add further tests to find bug in vectorised computations on LCLS example

--- tests/test_speed_optimizations.py
+++ tests/test_speed_optimizations.py
@@ -41,6 +41,44 @@ def test_merged_transfer_maps_tracking():
     assert torch.isclose(original_beam.total_charge, merged_beam.total_charge)
 
 
+def test_merged_transfer_maps_tracking_vectorized():
+    """
+    Test that tracking through merged transfer maps results in the same beam as the
+    original segment did in a vectorised setup.
+    """
+    incoming_beam = cheetah.ParameterBeam.from_astra(
+        "tests/resources/ACHIP_EA1_2021.1351.001"
+    ).broadcast((10,))
+
+    original_segment = cheetah.Segment(
+        elements=[
+            cheetah.Drift(length=torch.tensor([0.6])),
+            cheetah.Quadrupole(length=torch.tensor([0.2]), k1=torch.tensor([4.2])),
+            cheetah.Drift(length=torch.tensor([0.4])),
+            cheetah.HorizontalCorrector(
+                length=torch.tensor([0.1]), angle=torch.tensor([1e-4])
+            ),
+        ]
+    ).broadcast((10,))
+    merged_segment = original_segment.transfer_maps_merged(incoming_beam=incoming_beam)
+
+    original_beam = original_segment.track(incoming_beam)
+    merged_beam = merged_segment.track(incoming_beam)
+
+    assert torch.allclose(original_beam.mu_x, merged_beam.mu_x)
+    assert torch.allclose(original_beam.mu_xp, merged_beam.mu_xp)
+    assert torch.allclose(original_beam.mu_y, merged_beam.mu_y)
+    assert torch.allclose(original_beam.mu_yp, merged_beam.mu_yp)
+    assert torch.allclose(original_beam.sigma_x, merged_beam.sigma_x)
+    assert torch.allclose(original_beam.sigma_xp, merged_beam.sigma_xp)
+    assert torch.allclose(original_beam.sigma_y, merged_beam.sigma_y)
+    assert torch.allclose(original_beam.sigma_yp, merged_beam.sigma_yp)
+    assert torch.allclose(original_beam.sigma_s, merged_beam.sigma_s)
+    assert torch.allclose(original_beam.sigma_p, merged_beam.sigma_p)
+    assert torch.allclose(original_beam.energy, merged_beam.energy)
+    assert torch.allclose(original_beam.total_charge, merged_beam.total_charge)
+
+
 def test_merged_transfer_maps_num_elements():
     """
     Test that merging transfer maps results in the correct number of elements.
