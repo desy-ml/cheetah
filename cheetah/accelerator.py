6b5ac7c66fee4dcd4a50117c09a83a12aeaa4898
Jan Kaiser <jan.kaiser.email@googlemail.com>
2024-02-14 16:08:15 +0100

    Prevent flooding of unique element names by automatic optimisations

--- cheetah/accelerator.py
+++ cheetah/accelerator.py
@@ -2256,7 +2256,7 @@ class Segment(Element):
                 if not element.is_skippable:
                     todos.append(element)
                 elif not todos or not todos[-1].is_skippable:
-                    todos.append(Segment([element]))
+                    todos.append(Segment([element], name="temporary_todo"))
                 else:
                     todos[-1].elements.append(element)
 
