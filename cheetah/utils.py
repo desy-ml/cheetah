6efef53adcbde72a0123e6a66b83e3690480807f
Chenran Xu <chenran.xu@kit.edu>
2024-04-29 11:07:51 +0200

    Improve variable naming and docstring in ellispoid generation

--- cheetah/utils.py
+++ cheetah/utils.py
@@ -15,16 +15,20 @@ class UniqueNameGenerator:
 
 
 def generate_3d_uniform_ellipsoid(
-    num_particles: torch.Tensor, r_x: torch.Tensor, r_y: torch.Tensor, r_z: torch.Tensor
+    num_particles: torch.Tensor,
+    radius_x: torch.Tensor,
+    radius_y: torch.Tensor,
+    radius_s: torch.Tensor,
 ) -> torch.Tensor:
     """Generate the marcroparticles filling a 3D uniform ellipsoid.
     Note: Now we only consider non-batched version,
     e.g. num_particles, r_x, r_y, r_z are all length-1 tensors.
 
     :param num_particles: The number of macroparticles to be generated.
-    :param r_x: The radius of the ellipsoid along the x-axis.
-    :param r_y: The radius of the ellipsoid along the y-axis.
-    :param r_z: The radius of the ellipsoid along the z-axis.
+    :param radius_x: The radius of the ellipsoid along the x-axis in meters.
+    :param radius_y: The radius of the ellipsoid along the y-axis in meters.
+    :param radius_s: The radius of the ellipsoid along the s-axis
+        (longitudinal) in meters.
     :return: Phase-space coordinates of the particles
         with the shape of (1, num_particles, 7).
     """
@@ -34,12 +38,14 @@ def generate_3d_uniform_ellipsoid(
     num_generated = 0
 
     while num_generated < num_particles:
-        Xs = (torch.rand(num_particles) - 0.5) * 2 * r_x
-        Ys = (torch.rand(num_particles) - 0.5) * 2 * r_y
-        Zs = (torch.rand(num_particles) - 0.5) * 2 * r_z
+        Xs = (torch.rand(num_particles) - 0.5) * 2 * radius_x
+        Ys = (torch.rand(num_particles) - 0.5) * 2 * radius_y
+        Zs = (torch.rand(num_particles) - 0.5) * 2 * radius_s
 
         # Rejection sampling to get the points inside the ellipsoid.
-        indices = (Xs**2 / r_x**2 + Ys**2 / r_y**2 + Zs**2 / r_z**2) <= 1
+        indices = (
+            Xs**2 / radius_x**2 + Ys**2 / radius_y**2 + Zs**2 / radius_s**2
+        ) <= 1
 
         num_new_generated = Xs[indices].shape[0]
         num_to_add = min(num_new_generated, int(num_particles - num_generated))
--- tests/test_generation_uniform_ellispoid.py
+++ tests/test_generation_uniform_ellispoid.py
@@ -4,16 +4,18 @@ from cheetah.utils import generate_3d_uniform_ellipsoid
 
 
 def test_generate_uniform_ellipsoid():
-    r_x = torch.tensor(1e-3)
-    r_y = torch.tensor(1e-4)
-    r_z = torch.tensor(1e-5)
+    radius_x = torch.tensor(1e-3)
+    radius_y = torch.tensor(1e-4)
+    radius_s = torch.tensor(1e-5)
 
     num_particles = 1_000_000
-    particles = generate_3d_uniform_ellipsoid(num_particles, r_x, r_y, r_z)
+    particles = generate_3d_uniform_ellipsoid(
+        num_particles, radius_x, radius_y, radius_s
+    )
 
     assert particles.shape == (1, num_particles, 7)
     assert torch.all(particles[0, :, 6] == 1)
 
-    assert torch.max(torch.abs(particles[0, :, 0])) <= r_x
-    assert torch.max(torch.abs(particles[0, :, 2])) <= r_y
-    assert torch.max(torch.abs(particles[0, :, 4])) <= r_z
+    assert torch.max(torch.abs(particles[0, :, 0])) <= radius_x
+    assert torch.max(torch.abs(particles[0, :, 2])) <= radius_y
+    assert torch.max(torch.abs(particles[0, :, 4])) <= radius_s
