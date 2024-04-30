472fc02833075077ef718844b6a702204e567812
Jan Kaiser <jan.kaiser.email@googlemail.com>
2024-04-30 19:12:28 +0200

    Find test that breaks uniform beam distribution generation

--- tests/test_generation_uniform_ellispoid.py
+++ tests/test_generation_uniform_ellispoid.py
@@ -1,4 +1,5 @@
 import torch
+from icecream import ic
 
 from cheetah import ParticleBeam
 
@@ -32,24 +33,31 @@ def test_generate_uniform_ellipsoid_batched():
     radius_s = torch.tensor([1e-5, 2e-5])
 
     num_particles = torch.tensor(1_000_000)
-    parray = ParticleBeam.uniform_3d_ellispoid(
+    sigma_xp = torch.tensor([2e-7, 1e-7])
+    sigma_yp = torch.tensor([3e-7, 2e-7])
+    sigma_p = torch.tensor([0.000001, 0.000002])
+    energy = torch.tensor([1e7, 2e7])
+    total_charge = torch.tensor([1e-9, 3e-9])
+
+    num_particles = torch.tensor(1_000_000)
+    beam = ParticleBeam.uniform_3d_ellispoid(
         num_particles=num_particles,
         radius_x=radius_x,
         radius_y=radius_y,
         radius_s=radius_s,
+        sigma_xp=sigma_xp,
+        sigma_yp=sigma_yp,
+        sigma_p=sigma_p,
+        energy=energy,
+        total_charge=total_charge,
     )
 
-    particles = parray.particles
-
-    assert particles.shape == (2, 1_000_000, 7)
-    assert torch.all(particles[:, :, 6] == 1)
-
-    assert torch.all(
-        torch.max(torch.abs(particles[:, :, 0]), dim=-1).values <= radius_x
-    )
-    assert torch.all(
-        torch.max(torch.abs(particles[:, :, 2]), dim=-1).values <= radius_y
-    )
-    assert torch.all(
-        torch.max(torch.abs(particles[:, :, 4]), dim=-1).values <= radius_s
-    )
+    assert beam.num_particles == num_particles
+    assert torch.all(beam.xs.abs().transpose(0, 1) <= radius_x)
+    assert torch.all(beam.ys.abs().transpose(0, 1) <= radius_y)
+    assert torch.all(beam.ss.abs().transpose(0, 1) <= radius_s)
+    assert torch.allclose(beam.sigma_xp, sigma_xp)
+    assert torch.allclose(beam.sigma_yp, sigma_yp)
+    assert torch.allclose(beam.sigma_p, sigma_p)
+    assert torch.allclose(beam.energy, energy)
+    assert torch.allclose(beam.total_charge, total_charge)
