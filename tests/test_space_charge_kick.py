85a6edd780bb1fa611559b13829e46e1c1198c5e
greglenerd <gregoire_charleux@berkeley.edu>
2024-05-07 14:31:53 -0700

    new test, little change in accelerator.py

--- tests/test_space_charge_kick.py
+++ tests/test_space_charge_kick.py
@@ -4,7 +4,11 @@ import cheetah
 
 def test_cold_uniform_beam_expansion():
     """
-    Test that that a cold uniform beam doubles in size in both dimensions when travelling through a drift section with space_charge. (cf ImpactX test)
+    Tests that that a cold uniform beam doubles in size in both dimensions when 
+    travelling through a drift section with space_charge. (cf ImpactX test: 
+    https://impactx.readthedocs.io/en/latest/usage/examples/cfchannel/README.html#constant-focusing-channel-with-space-charge)
+    See Free Expansion of a Cold Uniform Bunch in 
+    https://accelconf.web.cern.ch/hb2023/papers/thbp44.pdf.
     """
 
     # Simulation parameters
@@ -20,7 +24,7 @@ def test_cold_uniform_beam_expansion():
         energy = energy,
         radius_x = R0,
         radius_y = R0,
-        radius_s = R0/gamma,  # radius of the beam in s direction, in the lab frame
+        radius_s = R0/gamma,  # radius of the beam in s direction, in the lab frame.
         sigma_xp = torch.tensor(1e-15),
         sigma_yp = torch.tensor(1e-15),
         sigma_p = torch.tensor(1e-15),
@@ -32,7 +36,7 @@ def test_cold_uniform_beam_expansion():
     sig_si = incoming.sigma_s
 
     # Compute section lenght
-    kappa = 1+(torch.sqrt(torch.tensor(2))/4)*torch.log(3+2*torch.sqrt(torch.tensor(2)))
+    kappa= 1+(torch.sqrt(torch.tensor(2))/4)*torch.log(3+2*torch.sqrt(torch.tensor(2)))
     Nb = total_charge/cheetah.elementary_charge
     L=beta*gamma*kappa*torch.sqrt(R0**3/(Nb*cheetah.electron_radius))
 
@@ -58,3 +62,37 @@ def test_cold_uniform_beam_expansion():
     assert torch.isclose(sig_xo,2*sig_xi,rtol=2e-2,atol=0.0)
     assert torch.isclose(sig_yo,2*sig_yi,rtol=2e-2,atol=0.0)
     assert torch.isclose(sig_so,2*sig_si,rtol=2e-2,atol=0.0)
+
+def test_incoming_beam_not_modified():
+    """
+    Tests that the incoming beam is not modified when calling the track method.
+    """
+
+    incoming_beam = cheetah.ParticleBeam.from_parameters(
+        num_particles=torch.tensor(10000),
+        sigma_xp=torch.tensor(2e-7),
+        sigma_yp=torch.tensor(2e-7),
+    )
+    # Initial beam properties
+    incoming_particles0 = incoming_beam.particles
+
+    L=torch.tensor(1.0)
+    segment_space_charge = cheetah.Segment(
+    elements=[
+        cheetah.Drift(L/6),
+        cheetah.SpaceChargeKick(L/3),
+        cheetah.Drift(L/3),
+        cheetah.SpaceChargeKick(L/3),
+        cheetah.Drift(L/3),
+        cheetah.SpaceChargeKick(L/3),
+        cheetah.Drift(L/6)
+    ]
+    )
+    # Calling the track method
+    outgoing_beam = segment_space_charge.track(incoming_beam) 
+    
+    # Final beam properties
+    incoming_particles1 = incoming_beam.particles
+
+    torch.set_printoptions(precision=16)
+    assert torch.allclose(incoming_particles0,incoming_particles1)
