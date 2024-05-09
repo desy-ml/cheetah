b43362799201d3c837843d40cab693a3be2e017d
Remi Lehe <remi.lehe@normalesup.org>
2024-05-09 06:34:01 -0700

    Fix test

--- tests/test_space_charge_kick.py
+++ tests/test_space_charge_kick.py
@@ -4,10 +4,10 @@ import cheetah
 
 def test_cold_uniform_beam_expansion():
     """
-    Tests that that a cold uniform beam doubles in size in both dimensions when 
-    travelling through a drift section with space_charge. (cf ImpactX test: 
+    Tests that that a cold uniform beam doubles in size in both dimensions when
+    travelling through a drift section with space_charge. (cf ImpactX test:
     https://impactx.readthedocs.io/en/latest/usage/examples/cfchannel/README.html#constant-focusing-channel-with-space-charge)
-    See Free Expansion of a Cold Uniform Bunch in 
+    See Free Expansion of a Cold Uniform Bunch in
     https://accelconf.web.cern.ch/hb2023/papers/thbp44.pdf.
     """
 
@@ -51,13 +51,13 @@ def test_cold_uniform_beam_expansion():
         cheetah.Drift(L/6)
     ]
     )
-    outgoing_beam = segment_space_charge.track(incoming) 
+    outgoing_beam = segment_space_charge.track(incoming)
 
     # Final beam properties
     sig_xo = outgoing_beam.sigma_x
     sig_yo = outgoing_beam.sigma_y
     sig_so = outgoing_beam.sigma_s
-    
+
     torch.set_printoptions(precision=16)
     assert torch.isclose(sig_xo,2*sig_xi,rtol=2e-2,atol=0.0)
     assert torch.isclose(sig_yo,2*sig_yi,rtol=2e-2,atol=0.0)
@@ -76,7 +76,7 @@ def test_incoming_beam_not_modified():
     # Initial beam properties
     incoming_particles0 = incoming_beam.particles
 
-    L=torch.tensor(1.0)
+    L=torch.tensor([1.0])
     segment_space_charge = cheetah.Segment(
     elements=[
         cheetah.Drift(L/6),
@@ -89,8 +89,8 @@ def test_incoming_beam_not_modified():
     ]
     )
     # Calling the track method
-    outgoing_beam = segment_space_charge.track(incoming_beam) 
-    
+    outgoing_beam = segment_space_charge.track(incoming_beam)
+
     # Final beam properties
     incoming_particles1 = incoming_beam.particles
 
