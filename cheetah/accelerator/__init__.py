efd5753b205265d38c75632f98381dbe72402d26
Remi Lehe <remi.lehe@normalesup.org>
2024-05-31 05:38:10 -0700

    Fix CI

--- cheetah/accelerator/__init__.py
+++ cheetah/accelerator/__init__.py
@@ -12,5 +12,6 @@ from .rbend import RBend  # noqa: F401
 from .screen import Screen  # noqa: F401
 from .segment import Segment  # noqa: F401
 from .solenoid import Solenoid  # noqa: F401
+from .space_charge_kick import SpaceChargeKick  # noqa: F401
 from .undulator import Undulator  # noqa: F401
 from .vertical_corrector import VerticalCorrector  # noqa: F401
