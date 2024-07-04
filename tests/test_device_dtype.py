24963cde3b6698f173652521a2dc1ba3a4fe7d05
Jan Kaiser <jan.kaiser.email@googlemail.com>
2024-07-04 15:43:32 +0200

    Add a `device` and `dtype` modification test for `ParticleBeam`

--- tests/test_device_dtype.py
+++ tests/test_device_dtype.py
@@ -65,3 +65,56 @@ def test_change_quadrupole_dtype():
     assert quad.k1.dtype == torch.float64
     assert quad.misalignment.dtype == torch.float64
     assert quad.tilt.dtype == torch.float64
+
+
+@pytest.mark.parametrize(
+    "target_device",
+    [
+        pytest.param(
+            torch.device("cuda"),
+            marks=pytest.mark.skipif(
+                not torch.cuda.is_available(), reason="CUDA not available"
+            ),
+        ),
+        pytest.param(
+            torch.device("mps"),
+            marks=pytest.mark.skipif(
+                not torch.backends.mps.is_available(), reason="MPS not available"
+            ),
+        ),
+    ],
+)
+def test_move_particlebeam_to_device(target_device: torch.device):
+    """Test that a particle beam can be successfully moved to a different device."""
+    beam = cheetah.ParticleBeam.from_parameters(num_particles=100_000)
+
+    # Test that by default the particle beam is on the CPU
+    assert beam.particles.device.type == "cpu"
+    assert beam.energy.device.type == "cpu"
+    assert beam.total_charge.device.type == "cpu"
+
+    beam.to(target_device)
+
+    # Test that the particle beam is now on the target device
+    assert beam.particles.device.type == target_device.type
+    assert beam.energy.device.type == target_device.type
+    assert beam.total_charge.device.type == target_device.type
+
+
+def test_change_particlebeam_dtype():
+    """
+    Test that a particle beam can be successfully changed to a different dtype.
+    """
+    beam = cheetah.ParticleBeam.from_parameters(num_particles=100_000)
+
+    # Test that by default the particle beam is of dtype float32
+    assert beam.particles.dtype == torch.float32
+    assert beam.energy.dtype == torch.float32
+    assert beam.total_charge.dtype == torch.float32
+
+    beam.to(torch.float64)
+
+    # Test that the particle beam is now of dtype float64
+    assert beam.particles.dtype == torch.float64
+    assert beam.energy.dtype == torch.float64
+    assert beam.total_charge.dtype == torch.float64
