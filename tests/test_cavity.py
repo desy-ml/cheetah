fecaa96c03a1db0cc53cc8f0abfe8163f08cfedb
Jan Kaiser <jan.kaiser@desy.de>
2024-07-10 10:58:17 +0200

    Fix `Cavity` being broken by vectorisation (#215)
    * Add test that detects bug in vectorised cavity with 0.0 voltage
    
    * Add non-working draft on fixing vectorised cavity bug
    
    * Fix vectorised cavity zero volatage bug
    
    * Clean up test that detected vectorised cavity zero volatage bug
    
    * Remove missed `icecream` import
    
    * Update changelog
    
    * Update test to capture more cases
    
    ---------
    
    Co-authored-by: Chenran Xu <chenran.xu@kit.edu>

--- tests/test_cavity.py
+++ tests/test_cavity.py
@@ -1,3 +1,4 @@
+import pytest
 import torch
 
 import cheetah
@@ -29,3 +30,51 @@ def test_assert_ei_greater_zero():
     ).broadcast((3,))
 
     _ = cavity.track(beam)
+
+
+@pytest.mark.parametrize(
+    "voltage",
+    [torch.tensor([0.0, 0.0]), torch.tensor([0.0, 1e6]), torch.tensor([1e6, 1e6])],
+)
+def test_vectorized_cavity_zero_voltage(voltage):
+    """
+    Tests that a vectorised cavity with zero voltage does not produce NaNs and that
+    zero voltage can be batched with non-zero voltage.
+
+    This was a bug introduced during the vectorisation of Cheetah, when the special
+    case of zero was removed and the `_cavity_rmatrix` method was also used in the case
+    of zero voltage. The latter produced NaNs in the transfer matrix when the voltage
+    is zero.
+    """
+    cavity = cheetah.Cavity(
+        length=torch.tensor([3.0441, 3.0441]),
+        voltage=voltage,
+        phase=torch.tensor([-0.0, -0.0]),
+        frequency=torch.tensor([2.8560e09, 2.8560e09]),
+        name="k27_1a",
+        dtype=torch.float64,
+    )
+    incoming = cheetah.ParameterBeam.from_parameters(
+        mu_x=torch.tensor([0.0]),
+        mu_xp=torch.tensor([0.0]),
+        mu_y=torch.tensor([0.0]),
+        mu_yp=torch.tensor([0.0]),
+        sigma_x=torch.tensor([4.8492e-06]),
+        sigma_xp=torch.tensor([1.5603e-07]),
+        sigma_y=torch.tensor([4.1209e-07]),
+        sigma_yp=torch.tensor([1.1035e-08]),
+        sigma_s=torch.tensor([1.0000e-10]),
+        sigma_p=torch.tensor([1.0000e-06]),
+        energy=torch.tensor([8.0000e09]),
+        total_charge=torch.tensor([0.0]),
+        dtype=torch.float64,
+    ).broadcast((2,))
+
+    outgoing = cavity.track(incoming)
+
+    assert not torch.isnan(cavity.transfer_map(incoming.energy)).any()
+
+    assert not torch.isnan(outgoing.sigma_x).any()
+    assert not torch.isnan(outgoing.sigma_y).any()
+    assert not torch.isnan(outgoing.beta_x).any()
+    assert not torch.isnan(outgoing.beta_y).any()
