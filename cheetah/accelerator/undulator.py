remove broadcasting methods, fix vectorized test

cheetah/accelerator/undulator.py
@@ -4,10 +4,9 @@ import matplotlib.pyplot as plt
 import torch
 from matplotlib.patches import Rectangle
 from scipy.constants import physical_constants
-from torch import Size, nn
+from torch import nn
 
 from cheetah.utils import UniqueNameGenerator
-
 from .element import Element
 
 generate_unique_name = UniqueNameGenerator(prefix="unnamed_element")
@@ -60,13 +59,6 @@ class Undulator(Element):
         tm[..., 4, 5] = self.length * igamma2
 
         return tm
-
-    def broadcast(self, shape: Size) -> Element:
-        return self.__class__(
-            length=self.length.repeat(shape),
-            is_active=self.is_active,
-            name=self.name,
-        )
 
     @property
     def is_skippable(self) -> bool:

tests/test_vectorized.py
@@ -1,5 +1,6 @@
-import cheetah
 import torch
+
+import cheetah
 
 from .resources import ARESlatticeStage3v1_9 as ares
 
@@ -393,7 +394,8 @@ def test_broadcast_customtransfermap():
     )
 
     element = cheetah.CustomTransferMap(
-        length=torch.tensor([0.4]), transfer_map=tm.repeat((3, 10))
+        length=torch.tensor([0.4]).repeat((3, 10)),
+        transfer_map=tm.repeat((3, 10, 1, 1)),
     )
     broadcast_element = element
 