implement batch calculation utility and fix vectorize tests

cheetah/accelerator/horizontal_corrector.py
@@ -7,6 +7,7 @@ from matplotlib.patches import Rectangle
 from torch import nn
 
 from cheetah.utils import UniqueNameGenerator
+from ..utils.batching import get_batch_shape
 
 from ..utils.physics import calculate_relativistic_factors
 from .element import Element
@@ -52,7 +53,8 @@ class HorizontalCorrector(Element):
 
         _, igamma2, beta = calculate_relativistic_factors(energy)
 
-        tm = torch.eye(7, device=device, dtype=dtype).repeat((*self.length.shape, 1, 1))
+        batch_shape = get_batch_shape(self.length, self.angle, beta)
+        tm = torch.eye(7, device=device, dtype=dtype).repeat((*batch_shape, 1, 1))
         tm[..., 0, 1] = self.length
         tm[..., 1, 6] = self.angle
         tm[..., 2, 3] = self.length

cheetah/track_methods.py
@@ -4,6 +4,7 @@ from typing import Optional
 
 import torch
 
+from cheetah.utils.batching import get_batch_shape
 from cheetah.utils.physics import calculate_relativistic_factors
 
 
@@ -72,7 +73,8 @@ def base_rmatrix(
 
     r56 = r56 - length / beta**2 * igamma2
 
-    R = torch.eye(7, dtype=dtype, device=device).repeat(*cx.shape, 1, 1)
+    batch_shape = get_batch_shape(dx, sx, beta, cx)
+    R = torch.eye(7, dtype=dtype, device=device).repeat(*batch_shape, 1, 1)
     R[..., 0, 0] = cx
     R[..., 0, 1] = sx
     R[..., 0, 5] = dx / beta

tests/test_reading_nx_tables.py
@@ -24,4 +24,4 @@ def test_length():
     """
     segment = cheetah.Segment.from_nx_tables("tests/resources/Stage4v3_9.txt")
 
-    assert segment.length == torch.tensor(44.2215)
+    assert torch.allclose(segment.length, torch.tensor([44.2215]))

tests/test_space_charge_kick.py
@@ -246,7 +246,7 @@ def test_does_not_break_segment_length():
     Test that the computation of a `Segment`'s length does not break when
     `SpaceChargeKick` is used.
     """
-    section_length = torch.tensor([1.0])
+    section_length = torch.tensor([1.0]).repeat((3,2))
     segment = cheetah.Segment(
         elements=[
             cheetah.Drift(section_length / 6),
@@ -257,7 +257,7 @@ def test_does_not_break_segment_length():
             cheetah.SpaceChargeKick(section_length / 3),
             cheetah.Drift(section_length / 6),
         ]
-    ).broadcast(shape=(3, 2))
+    )
 
     assert segment.length.shape == (3, 2)
     assert torch.allclose(segment.length, torch.tensor([1.0]).repeat(3, 2))