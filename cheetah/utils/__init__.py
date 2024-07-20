dd0ecf12384b998ae7439287379ac0de07764979
Jan Kaiser <jan.kaiser.email@googlemail.com>
2024-07-20 12:00:07 +0200

    Fix import error

--- cheetah/utils/__init__.py
+++ cheetah/utils/__init__.py
@@ -1,4 +1,3 @@
-import bmadx  # noqa: F401
-
+from . import bmadx  # noqa: F401
 from .kde import kde_histogram_1d, kde_histogram_2d  # noqa: F401
 from .unique_name_generator import UniqueNameGenerator  # noqa: F401
