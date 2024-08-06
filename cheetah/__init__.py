bfd030e75bc2aecfce17706773cd7d5ea6067f59
jp-ga <97850904+jp-ga@users.noreply.github.com>
2024-08-06 11:06:03 -0500

    run isort

--- cheetah/__init__.py
+++ cheetah/__init__.py
@@ -1,21 +1,7 @@
 import cheetah.converters  # noqa: F401
-from cheetah.accelerator import (
-    BPM,
-    Aperture,
-    Cavity,  # noqa: F401
-    CrabCavity,
-    CustomTransferMap,
-    Dipole,
-    Drift,
-    HorizontalCorrector,
-    Marker,
-    Quadrupole,
-    RBend,
-    Screen,
-    Segment,
-    Solenoid,
-    SpaceChargeKick,
-    Undulator,
-    VerticalCorrector,
-)
+from cheetah.accelerator import Cavity  # noqa: F401
+from cheetah.accelerator import (BPM, Aperture, CrabCavity, CustomTransferMap,
+                                 Dipole, Drift, HorizontalCorrector, Marker,
+                                 Quadrupole, RBend, Screen, Segment, Solenoid,
+                                 SpaceChargeKick, Undulator, VerticalCorrector)
 from cheetah.particles import ParameterBeam, ParticleBeam  # noqa: F401
