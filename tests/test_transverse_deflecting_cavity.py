cdd248041bf054d367423ea91be158291f3c2ca3
jp-ga <97850904+jp-ga@users.noreply.github.com>
2024-08-20 03:11:23 -0500

    update tests

--- tests/test_transverse_deflecting_cavity.py
+++ tests/test_transverse_deflecting_cavity.py
@@ -23,10 +23,10 @@ def test_transverse_deflecting_cavity_bmadx_tracking():
     outgoing_beam = tdc.track(incoming_beam)
 
     # Load reference result computed with Bmad-X
-    outgoing_beam_bmadx = torch.load(
-        "tests/resources/bmadx/outgoing_beam_bmadx_crab_cavity.pt"
+    outgoing_bmadx = torch.load(
+        "tests/resources/bmadx/outgoing_bmadx_crab_cavity.pt"
     )
 
     assert torch.allclose(
-        outgoing_beam.particles, outgoing_beam_bmadx.particles, atol=1e-14, rtol=1e-14
+        outgoing_beam.particles, outgoing_bmadx, atol=1e-14, rtol=1e-14
     )
