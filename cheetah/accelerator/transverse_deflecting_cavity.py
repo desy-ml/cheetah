fix double precision issues

cheetah/accelerator/transverse_deflecting_cavity.py
@@ -15,7 +15,8 @@ from .element import Element
 generate_unique_name = UniqueNameGenerator(prefix="unnamed_element")
 
 electron_mass_eV = torch.tensor(
-    physical_constants["electron mass energy equivalent in MeV"][0] * 1e6
+    physical_constants["electron mass energy equivalent in MeV"][0] * 1e6,
+    dtype=torch.float64,
 )
 
 

tests/test_quadrupole.py
@@ -141,10 +141,10 @@ def test_quadrupole_bmadx_tracking():
     """
     incoming = torch.load("tests/resources/bmadx/incoming_beam.pt")
     quadrupole = Quadrupole(
-        length=torch.tensor([1.0]),
-        k1=torch.tensor([10.0]),
-        misalignment=torch.tensor([[0.01, -0.02]]),
-        tilt=torch.tensor([0.5]),
+        length=torch.tensor([1.0], dtype=torch.double),
+        k1=torch.tensor([10.0], dtype=torch.double),
+        misalignment=torch.tensor([[0.01, -0.02]], dtype=torch.double),
+        tilt=torch.tensor([0.5], dtype=torch.double),
         num_steps=10,
         tracking_method="bmadx",
         dtype=torch.double,

tests/test_transverse_deflecting_cavity.py
@@ -11,10 +11,10 @@ def test_transverse_deflecting_cavity_bmadx_tracking():
     """
     incoming_beam = torch.load("tests/resources/bmadx/incoming_beam.pt")
     tdc = cheetah.TransverseDeflectingCavity(
-        length=torch.tensor([1.0]),
-        voltage=torch.tensor([1e7]),
-        phase=torch.tensor([0.2]),
-        frequency=torch.tensor([1e9]),
+        length=torch.tensor([1.0], dtype=torch.double),
+        voltage=torch.tensor([1e7], dtype=torch.double),
+        phase=torch.tensor([0.2], dtype=torch.double),
+        frequency=torch.tensor([1e9], dtype=torch.double),
         tracking_method="bmadx",
         dtype=torch.double,
     )
@@ -28,5 +28,5 @@ def test_transverse_deflecting_cavity_bmadx_tracking():
     )
 
     assert torch.allclose(
-        outgoing_beam.particles, outgoing_beam_bmadx.particles, atol=1e-7, rtol=1e-7
+        outgoing_beam.particles, outgoing_beam_bmadx.particles, atol=0, rtol=1e-14
     )