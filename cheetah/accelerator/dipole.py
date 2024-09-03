b1e59969f4a8710112aedd36468eb2547c2b47bf
Juan Pablo Gonzalez-Aguilera <97850904+jp-ga@users.noreply.github.com>
2024-09-03 20:00:08 +0000

    dipole reference momentum fix

--- cheetah/accelerator/dipole.py
+++ cheetah/accelerator/dipole.py
@@ -23,7 +23,6 @@ class Dipole(Element):
     Dipole magnet (by default a sector bending magnet).
 
     :param length: Length in meters.
-    :param p0c: Reference momentum at dipole in eV/c.
     :param angle: Deflection angle in rad.
     :param k1: Focussing strength in 1/m^-2.
     :param e1: The angle of inclination of the entrance face [rad].
@@ -47,7 +46,6 @@ class Dipole(Element):
     def __init__(
         self,
         length: Union[torch.Tensor, nn.Parameter],
-        p0c: Optional[Union[torch.Tensor, nn.Parameter]] = None,
         angle: Optional[Union[torch.Tensor, nn.Parameter]] = None,
         k1: Optional[Union[torch.Tensor, nn.Parameter]] = None,
         e1: Optional[Union[torch.Tensor, nn.Parameter]] = None,
@@ -68,14 +66,6 @@ class Dipole(Element):
         super().__init__(name=name)
 
         self.register_buffer("length", torch.as_tensor(length, **factory_kwargs))
-        self.register_buffer(
-            "p0c",
-            (
-                torch.as_tensor(p0c, **factory_kwargs)
-                if p0c is not None
-                else torch.zeros_like(self.length)
-            ),
-        )
         self.register_buffer(
             "angle",
             (
@@ -204,7 +194,7 @@ class Dipole(Element):
         tau = incoming.tau
         delta = incoming.p
 
-        z, pz, p0c_particle = bmadx.cheetah_to_bmad_z_pz(
+        z, pz, p0c = bmadx.cheetah_to_bmad_z_pz(
             tau, delta, incoming.energy, electron_mass_eV
         )
 
@@ -215,13 +205,13 @@ class Dipole(Element):
 
         if self.fringe_at == "entrance" or self.fringe_at == "both":
             px, py = self._bmadx_fringe_linear(
-                "entrance", x, px, y, py, pz, p0c_particle
+                "entrance", x, px, y, py, pz, p0c
             )
         x, px, y, py, z, pz = self._bmadx_body(
-            x, px, y, py, z, pz, p0c_particle, electron_mass_eV
+            x, px, y, py, z, pz, p0c, electron_mass_eV
         )
         if self.fringe_at == "exit" or self.fringe_at == "both":
-            px, py = self._bmadx_fringe_linear("exit", x, px, y, py, pz, p0c_particle)
+            px, py = self._bmadx_fringe_linear("exit", x, px, y, py, pz, p0c)
 
         x, px, y, py = bmadx.offset_particle_unset(
             torch.tensor(0.0), torch.tensor(0.0), self.tilt, x, px, y, py
@@ -230,7 +220,7 @@ class Dipole(Element):
 
         # Convert back to Cheetah coordinates
         tau, delta, ref_energy = bmadx.bmad_to_cheetah_z_pz(
-            z, pz, p0c_particle, electron_mass_eV
+            z, pz, p0c, electron_mass_eV
         )
 
         outgoing_beam = ParticleBeam(
@@ -242,10 +232,7 @@ class Dipole(Element):
         )
         return outgoing_beam
 
-    def _bmadx_body(self, x, px, y, py, z, pz, p0c_particle, mc2):
-        px = px * p0c_particle / self.p0c
-        py = py * p0c_particle / self.p0c
-        pz = (pz + 1) * p0c_particle / self.p0c - 1
+    def _bmadx_body(self, x, px, y, py, z, pz, p0c, mc2):
 
         px_norm = torch.sqrt((1 + pz) ** 2 - py**2)  # For simplicity
         phi1 = torch.arcsin(px / px_norm)
@@ -282,11 +269,11 @@ class Dipole(Element):
         Lc = torch.sqrt(Lcu**2 + Lcv**2)
         Lp = Lc / bmadx.sinc(theta_p / 2)
 
-        P = self.p0c * (1 + pz)  # In eV
+        P = p0c * (1 + pz)  # In eV
         E = torch.sqrt(P**2 + mc2**2)  # In eV
-        E0 = torch.sqrt(self.p0c**2 + mc2**2)  # In eV
+        E0 = torch.sqrt(p0c**2 + mc2**2)  # In eV
         beta = P / E
-        beta0 = self.p0c / E0
+        beta0 = p0c / E0
 
         x_f = x2
         px_f = px_norm * torch.sin(self.angle + phi1 - theta_p)
@@ -303,11 +290,8 @@ class Dipole(Element):
         y: Union[torch.Tensor, nn.Parameter],
         py: Union[torch.Tensor, nn.Parameter],
         pz: Union[torch.Tensor, nn.Parameter],
-        p0c_particle: Union[torch.Tensor, nn.Parameter],
+        p0c: Union[torch.Tensor, nn.Parameter],
     ):
-        px = px * p0c_particle / self.p0c
-        py = py * p0c_particle / self.p0c
-        pz = (pz + 1) * p0c_particle / self.p0c - 1
         g = self.angle / self.length
         e = self.e1 * (location == "entrance") + self.e2 * (location == "exit")
         f_int = self.fringe_integral * (
@@ -421,7 +405,6 @@ class Dipole(Element):
     def __repr__(self):
         return (
             f"{self.__class__.__name__}(length={repr(self.length)}, "
-            + f"p0c={repr(self.p0c)}, "
             + f"angle={repr(self.angle)}, "
             + f"k1={repr(self.k1)}, "
             + f"e1={repr(self.e1)},"
@@ -441,7 +424,6 @@ class Dipole(Element):
     def defining_features(self) -> list[str]:
         return super().defining_features + [
             "length",
-            "p0c",
             "angle",
             "k1",
             "e1",
