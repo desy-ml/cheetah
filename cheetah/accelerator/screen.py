fix misalignment broadcasting issue with parameter beam
expands the shape of mu tensor to make sure it is at least 2d (adds a batch dimension to the tensor if 1d)

cheetah/accelerator/screen.py
@@ -162,14 +162,16 @@ class Screen(Element):
             cin = deepcopy(incoming)
 
             if isinstance(incoming, ParameterBeam):
-                cin._mu = torch.broadcast_to(
-                    cin._mu, (*self.misalignment.shape[:-1], 7)
+                cin._mu = cin._mu.broadcast_to(
+                    self.misalignment.shape[:-1] + cin._mu.shape
                 ).clone()
-                cin._mu[..., 0] -= self.misalignment[..., 0]
-                cin._mu[..., 2] -= self.misalignment[..., 1]
+
+                cin._mu[..., 0] -= self.misalignment[..., 0].unsqueeze(-1)
+                cin._mu[..., 2] -= self.misalignment[..., 1].unsqueeze(-1)
+
             elif isinstance(incoming, ParticleBeam):
                 cin.particles = cin.particles.broadcast_to(
-                    self.misalignment[..., 0].shape + cin.particles.shape
+                    self.misalignment.shape[:-1] + cin.particles.shape
                 ).clone()
 
                 cin.particles[..., 0] -= self.misalignment[..., 0].unsqueeze(-1)

cheetah/particles/parameter_beam.py
@@ -30,7 +30,9 @@ class ParameterBeam(Beam):
         factory_kwargs = {"device": device, "dtype": dtype}
         super().__init__()
 
-        self.register_buffer("_mu", torch.as_tensor(mu, **factory_kwargs))
+        self.register_buffer(
+            "_mu", torch.atleast_2d(torch.as_tensor(mu, **factory_kwargs))
+        )
         self.register_buffer("_cov", torch.as_tensor(cov, **factory_kwargs))
         total_charge = (
             total_charge