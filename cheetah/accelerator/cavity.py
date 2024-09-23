Fix one of the failing tests; remove old broadcast methods

cheetah/accelerator/cavity.py
@@ -4,15 +4,17 @@ import matplotlib.pyplot as plt
 import torch
 from matplotlib.patches import Rectangle
 from scipy import constants
+from scipy.constants import physical_constants
 from torch import nn
 
 from ..particles import Beam, ParameterBeam, ParticleBeam
 from ..track_methods import base_rmatrix
-from ..utils import UniqueNameGenerator
-from ..utils.physics import compute_relativistic_factors, electron_mass_eV
+from ..utils import UniqueNameGenerator, compute_relativistic_factors
 from .element import Element
 
 generate_unique_name = UniqueNameGenerator(prefix="unnamed_element")
+
+electron_mass_eV = physical_constants["electron mass energy equivalent in MeV"][0] * 1e6
 
 
 class Cavity(Element):

cheetah/accelerator/drift.py
@@ -3,11 +3,10 @@ from typing import Literal, Optional, Union
 import matplotlib.pyplot as plt
 import torch
 from scipy.constants import physical_constants
-from torch import Size, nn
+from torch import nn
 
 from ..particles import Beam, ParticleBeam
-from ..utils import UniqueNameGenerator, bmadx
-from ..utils.physics import compute_relativistic_factors
+from ..utils import UniqueNameGenerator, bmadx, compute_relativistic_factors
 from .element import Element
 
 generate_unique_name = UniqueNameGenerator(prefix="unnamed_element")
@@ -115,15 +114,6 @@ class Drift(Element):
             dtype=incoming.particles.dtype,
         )
         return outgoing_beam
-
-    def broadcast(self, shape: Size) -> Element:
-        return self.__class__(
-            length=self.length.repeat(shape),
-            tracking_method=self.tracking_method,
-            name=self.name,
-            device=self.length.device,
-            dtype=self.length.dtype,
-        )
 
     @property
     def is_skippable(self) -> bool:

cheetah/accelerator/horizontal_corrector.py
@@ -6,8 +6,7 @@ import torch
 from matplotlib.patches import Rectangle
 from torch import nn
 
-from ..utils import UniqueNameGenerator
-from ..utils.physics import compute_relativistic_factors
+from ..utils import UniqueNameGenerator, compute_relativistic_factors
 from .element import Element
 
 generate_unique_name = UniqueNameGenerator(prefix="unnamed_element")
@@ -51,7 +50,8 @@ class HorizontalCorrector(Element):
 
         _, igamma2, beta = compute_relativistic_factors(energy)
 
-        vector_shape = torch.broadcast_tensors(self.length, self.angle, beta)[0].shape
+        vector_shape = torch.broadcast_shapes(self.length.shape, igamma2.shape)
+
         tm = torch.eye(7, device=device, dtype=dtype).repeat((*vector_shape, 1, 1))
         tm[..., 0, 1] = self.length
         tm[..., 1, 6] = self.angle