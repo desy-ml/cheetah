5d77330160d53ac8476fa21945b7a85476d34026
Christian Hespe <christian.hespe@desy.de>
2024-09-24 14:47:19 +0200

    Fix tests

--- tests/test_elegant_conversion.py
+++ tests/test_elegant_conversion.py
@@ -15,16 +15,16 @@ def test_fodo():
             cheetah.Quadrupole(
                 name="q1", length=torch.tensor([0.1]), k1=torch.tensor([1.5])
             ),
-            cheetah.Drift(name="d1", length=torch.tensor([1])),
+            cheetah.Drift(name="d1", length=torch.tensor([1.0])),
             cheetah.Marker(name="m1"),
             cheetah.Dipole(
                 name="s1", length=torch.tensor([0.3]), e1=torch.tensor([0.25])
             ),
-            cheetah.Drift(name="d1", length=torch.tensor([1])),
+            cheetah.Drift(name="d1", length=torch.tensor([1.0])),
             cheetah.Quadrupole(
-                name="q2", length=torch.tensor([0.2]), k1=torch.tensor([-3])
+                name="q2", length=torch.tensor([0.2]), k1=torch.tensor([-3.0])
             ),
-            cheetah.Drift(name="d2", length=torch.tensor([2])),
+            cheetah.Drift(name="d2", length=torch.tensor([2.0])),
         ],
         name="fodo",
     )
