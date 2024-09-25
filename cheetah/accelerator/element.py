Fix vectorisation issue in length computation with zero-length elements

cheetah/accelerator/element.py
@@ -22,7 +22,7 @@ class Element(ABC, nn.Module):
         super().__init__()
 
         self.name = name if name is not None else generate_unique_name()
-        self.register_buffer("length", torch.zeros((1,)))
+        self.register_buffer("length", torch.tensor(0.0))
 
     def transfer_map(self, energy: torch.Tensor) -> torch.Tensor:
         r"""