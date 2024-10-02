a6e1d5c07ec0adeef32ee1322cba4f8981151407
Jan Kaiser <jan.kaiser.email@googlemail.com>
2024-10-02 14:51:29 +0200

    Add reference particle plotting test case

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -1,3 +1,5 @@
+import torch
+
 import cheetah
 
 from .resources import ARESlatticeStage3v1_9 as ares
@@ -22,5 +24,36 @@ def test_twiss_plot():
     )
     segment = cheetah.Segment.from_ocelot(cell)
 
-    # Do the actual plotting
+    # Run the plotting to see if it raises an exception
     segment.plot_twiss(incoming_beam)
+
+
+def test_reference_particle_plot():
+    """
+    Test that the reference particle plot does not raise an exception using the example
+    from the `simple.ipynb` example notebook from the documentation.
+    """
+    segment = cheetah.Segment(
+        elements=[
+            cheetah.BPM(name="BPM1SMATCH"),
+            cheetah.Drift(length=torch.tensor(1.0)),
+            cheetah.BPM(name="BPM6SMATCH"),
+            cheetah.Drift(length=torch.tensor(1.0)),
+            cheetah.VerticalCorrector(length=torch.tensor(0.3), name="V7SMATCH"),
+            cheetah.Drift(length=torch.tensor(0.2)),
+            cheetah.HorizontalCorrector(length=torch.tensor(0.3), name="H10SMATCH"),
+            cheetah.Drift(length=torch.tensor(7.0)),
+            cheetah.HorizontalCorrector(length=torch.tensor(0.3), name="H12SMATCH"),
+            cheetah.Drift(length=torch.tensor(0.05)),
+            cheetah.BPM(name="BPM13SMATCH"),
+        ]
+    )
+
+    segment.V7SMATCH.angle = torch.tensor(3.142e-3)
+
+    incoming = cheetah.ParticleBeam.from_astra(
+        "tests/resources/ACHIP_EA1_2021.1351.001"
+    )
+
+    # Run the plotting to see if it raises an exception
+    segment.plot_overview(beam=incoming)
