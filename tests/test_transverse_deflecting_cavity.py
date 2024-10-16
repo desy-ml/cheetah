481e2497bb643aed47902312dbcd4e13157042ff
Jan Kaiser <jan.kaiser.email@googlemail.com>
2024-10-16 21:16:15 +0200

    Separate tests

--- tests/test_transverse_deflecting_cavity.py
+++ tests/test_transverse_deflecting_cavity.py
@@ -39,9 +39,10 @@ def test_transverse_deflecting_cavity_bmadx_tracking(dtype):
     )
 
 
-def test_transverse_deflecting_cavity_vectorisation():
+def test_transverse_deflecting_cavity_energy_length_vectorization():
     """
-    Test that the TDC supports vectroized tracking
+    Test that vectorised tracking through a TDC throws now exception and outputs the
+    correct shape, when the input beam's energy and the TDC's length are vectorised.
     """
     incoming_beam = cheetah.ParticleBeam.from_parameters(
         num_particles=torch.tensor(10_000),
@@ -49,7 +50,6 @@ def test_transverse_deflecting_cavity_vectorisation():
         sigma_py=torch.tensor(2e-7),
         energy=torch.tensor([50e6, 60e6]),
     )
-    # Vectorise voltage
     tdc = cheetah.TransverseDeflectingCavity(
         length=torch.tensor(1.0),
         voltage=torch.tensor([[1e7], [2e7], [3e7]]),
@@ -58,20 +58,46 @@ def test_transverse_deflecting_cavity_vectorisation():
         tracking_method="bmadx",
     )
 
-    # Run tracking
-    _ = tdc.track(incoming_beam)
+    outgoing_beam = tdc.track(incoming_beam)
 
-    # Vectorise phase
-    tdc2 = cheetah.TransverseDeflectingCavity(
+    assert outgoing_beam.particles.shape[:-2] == torch.Size([3, 2])
+
+
+def test_transverse_deflecting_cavity_energy_phase_vectorization():
+    """
+    Test that vectorised tracking through a TDC throws now exception and outputs the
+    correct shape, when the input beam's energy and the TDC's phase are vectorised.
+    """
+    incoming_beam = cheetah.ParticleBeam.from_parameters(
+        num_particles=torch.tensor(10_000),
+        sigma_px=torch.tensor(2e-7),
+        sigma_py=torch.tensor(2e-7),
+        energy=torch.tensor([50e6, 60e6]),
+    )
+    tdc = cheetah.TransverseDeflectingCavity(
         length=torch.tensor(1.0),
         voltage=torch.tensor(1e7),
         phase=torch.tensor([[0.6], [0.5], [0.4]]),
         frequency=torch.tensor(1e9),
         tracking_method="bmadx",
     )
-    _ = tdc2.track(incoming_beam)
 
-    # Vectorise frequency
+    outgoing_beam = tdc.track(incoming_beam)
+
+    assert outgoing_beam.particles.shape[:-2] == torch.Size([3, 2])
+
+
+def test_transverse_deflecting_cavity_energy_frequency_vectorization():
+    """
+    Test that vectorised tracking through a TDC throws now exception and outputs the
+    correct shape, when the input beam's energy and the TDC's frequency are vectorised.
+    """
+    incoming_beam = cheetah.ParticleBeam.from_parameters(
+        num_particles=torch.tensor(10_000),
+        sigma_px=torch.tensor(2e-7),
+        sigma_py=torch.tensor(2e-7),
+        energy=torch.tensor([50e6, 60e6]),
+    )
     tdc3 = cheetah.TransverseDeflectingCavity(
         length=torch.tensor(1.0),
         voltage=torch.tensor(1e7),
@@ -79,16 +105,31 @@ def test_transverse_deflecting_cavity_vectorisation():
         frequency=torch.tensor([[1e9], [2e9], [3e9]]),
         tracking_method="bmadx",
     )
+
     _ = tdc3.track(incoming_beam)
 
-    # Try vectorising all parameters
-    tdc4 = cheetah.TransverseDeflectingCavity(
+    assert _.particles.shape[:-2] == torch.Size([3, 2])
+
+
+def test_transverse_deflecting_cavity_all_parameters_vectorization():
+    """
+    Test that vectorised tracking through a TDC throws now exception and outputs the
+    correct shape, when all parameters are vectorised.
+    """
+    incoming_beam = cheetah.ParticleBeam.from_parameters(
+        num_particles=torch.tensor(10_000),
+        sigma_px=torch.tensor(2e-7),
+        sigma_py=torch.tensor(2e-7),
+        energy=torch.tensor([50e6, 60e6]),
+    )
+    tdc = cheetah.TransverseDeflectingCavity(
         length=torch.tensor(1.0),
         voltage=torch.ones([4, 1, 1, 1]) * 1e7,
         phase=torch.ones([1, 3, 1, 1]) * 0.4,
         frequency=torch.ones([1, 1, 2, 1]) * 1e9,
         tracking_method="bmadx",
     )
-    outgoing_beam = tdc4.track(incoming_beam)
+
+    outgoing_beam = tdc.track(incoming_beam)
 
     assert outgoing_beam.particles.shape[:-2] == torch.Size([4, 3, 2, 2])
