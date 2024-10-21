ba1de82389f73ea4e3f5c84aecfa73903be60e6b
Jan Kaiser <jan.kaiser.email@googlemail.com>
2024-10-21 02:16:23 +0200

    Fix up tests

--- tests/test_rpn.py
+++ tests/test_rpn.py
@@ -6,9 +6,20 @@ def test_valid_rpn_expression():
     Test that a valid RPN expression without nesting is correctly recognised as a valid
     RPN expression.
     """
-    expression = "'2 3 +'"
+    expression = "2 3 +"
 
     assert rpn.is_valid_expression(expression)
+
+
+def test_valid_rpn_expression_with_single_quotes():
+    """
+    Test that a valid RPN expression that has single quotes around it and should
+    therefore not be recognised as valid because these should have been stripped off
+    before calling the function.
+    """
+    expression = "'2 3 +'"
+
+    assert not rpn.is_valid_expression(expression)
 
 
 def test_falsely_validated_normal_expression():
@@ -17,6 +28,6 @@ def test_falsely_validated_normal_expression():
     falsely recognised as a valid RPN expression in a previous version of Cheetah, is
     correctly recognised as invalid.
     """
-    expression = "'ldsp2h +dldsp17h +lblxsph/2-lbxsph/2'"
+    expression = "ldsp2h +dldsp17h +lblxsph/2-lbxsph/2"
 
     assert not rpn.is_valid_expression(expression)
