3e4e7c8fa840991fd8f595e2691c9f6a792503bf
Chenran Xu <chenran.xu@kit.edu>
2024-10-24 17:46:39 +0200

    Include particle survival into the statistical beam parameters calculations

--- tests/test_compare_ocelot.py
+++ tests/test_compare_ocelot.py
@@ -223,6 +223,15 @@ def test_aperture_elliptical():
         == outgoing_p_array.rparticles.shape[1]
     )
 
+    assert np.allclose(
+        outgoing_beam.mu_x.cpu().numpy(),
+        outgoing_p_array.x().mean(),
+    )
+    assert np.allclose(
+        outgoing_beam.mu_px.cpu().numpy(),
+        outgoing_p_array.px().mean(),
+    )
+
 
 def test_solenoid():
     """
