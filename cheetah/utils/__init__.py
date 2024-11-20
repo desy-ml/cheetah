923a97de9b364df4abed2826cccf79f4d552fa6a
Jan Kaiser <jan.kaiser.email@googlemail.com>
2024-11-20 16:25:21 +0100

    Remove argument verification functions that are not needed anymore

--- cheetah/utils/__init__.py
+++ cheetah/utils/__init__.py
@@ -1,10 +1,5 @@
 from . import bmadx  # noqa: F401
-from .argument_verification import (  # noqa: F401
-    are_all_the_same_device,
-    are_all_the_same_dtype,
-    extract_argument_shape,
-    verify_device_and_dtype,
-)
+from .argument_verification import verify_device_and_dtype  # noqa: F401
 from .device import is_mps_available_and_functional  # noqa: F401
 from .elementwise_linspace import elementwise_linspace  # noqa: F401
 from .kde import kde_histogram_1d, kde_histogram_2d  # noqa: F401
