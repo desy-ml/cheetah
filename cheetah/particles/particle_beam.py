908f8bc8a9bb13eebdcdfbb9db7a2879bb0ec6c9
Ryan Roussel <rroussel@slac.stanford.edu>
2024-12-06 02:48:15 -0800

    `ParticleBeam.__getitem__` method (#291)
    * add get item method to particle beam
    
    * add test
    
    * Update .gitignore
    
    * handle indexing for beam energy and beam charge
    
    * make sure that the energy batch shape and particle batch shape match
    
    * formatting
    
    * Clean up .idea ignoring
    
    * Another `.gitignore` fix
    
    * Fix and split tests; add invalid index test
    
    * Reimplement fixed, simpler and more general `__getitem__`
    
    * Update changelog for `ParticleBeam.__getitem__`
    
    * Add type annotations
    
    ---------
    
    Co-authored-by: Jan Kaiser <jan.kaiser@desy.de>
    Co-authored-by: Jan Kaiser <jan.kaiser.email@googlemail.com>

--- cheetah/particles/particle_beam.py
+++ cheetah/particles/particle_beam.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, Union
 
 import torch
 from scipy import constants
@@ -1073,6 +1073,33 @@ class ParticleBeam(Beam):
     def momenta(self) -> torch.Tensor:
         """Momenta of the individual particles."""
         return torch.sqrt(self.energies**2 - electron_mass_eV**2)
+
+    def __getitem__(self, item: Union[int, slice, torch.Tensor]) -> "ParticleBeam":
+        vector_shape = torch.broadcast_shapes(
+            self.particles.shape[:-2],
+            self.energy.shape,
+            self.particle_charges.shape[:-1],
+            self.survival_probabilities.shape[:-1],
+        )
+        broadcasted_particles = torch.broadcast_to(
+            self.particles, (*vector_shape, self.num_particles, 7)
+        )
+        broadcasted_energy = torch.broadcast_to(self.energy, vector_shape)
+        broadcasted_particle_charges = torch.broadcast_to(
+            self.particle_charges, (*vector_shape, self.num_particles)
+        )
+        broadcasted_survival_probabilities = torch.broadcast_to(
+            self.survival_probabilities, (*vector_shape, self.num_particles)
+        )
+
+        return self.__class__(
+            particles=broadcasted_particles[item],
+            energy=broadcasted_energy[item],
+            particle_charges=broadcasted_particle_charges[item],
+            survival_probabilities=broadcasted_survival_probabilities[item],
+            device=self.particles.device,
+            dtype=self.particles.dtype,
+        )
 
     def __repr__(self) -> str:
         return (
