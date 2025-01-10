1c17f4b0a0c9924382cdcdd9ad4a34f9f3e515fb
Jan Kaiser <jan.kaiser.email@googlemail.com>
2025-01-10 15:59:21 +0100

    Clean up OpenPMD tests

--- tests/test_openpmd_conversion.py
+++ tests/test_openpmd_conversion.py
@@ -1,63 +1,67 @@
 import torch
 
-from cheetah import ParticleBeam
+import cheetah
 
 
 def test_particlebeam_to_and_from_particlegroup():
+    """
+    Test that a `ParticleBeam` can be converted to an OpenPMD `ParticleGroup` and back,
+    checking that the loaded `ParticleBeam` is the same as the original.
+    """
+    reference_energy = torch.tensor(1e6)
 
-    ref_energy = torch.tensor(1e6)
-    beam = ParticleBeam.from_parameters(
-        num_particles=10000,
+    original_cheetah_beam = cheetah.ParticleBeam.from_parameters(
+        num_particles=10_000,
         mu_x=torch.tensor(1e-4),
         sigma_x=torch.tensor(2e-5),
         mu_y=torch.tensor(1e-4),
         sigma_y=torch.tensor(2e-5),
         sigma_p=torch.tensor(1e-4),
-        energy=ref_energy,
+        energy=reference_energy,
         total_charge=torch.tensor(1e-9),
         dtype=torch.float64,
     )
-
-    particle_group = beam.to_openpmd_particlegroup()
-
-    # Test that the loaded particle group is the same as the original beam
-    beam_loaded = ParticleBeam.from_openpmd_particlegroup(
-        particle_group, energy=ref_energy, dtype=torch.float64
+    openpmd_particle_group = original_cheetah_beam.to_openpmd_particlegroup()
+    loaded_cheetah_beam = cheetah.ParticleBeam.from_openpmd_particlegroup(
+        openpmd_particle_group, energy=reference_energy, dtype=torch.float64
     )
 
-    assert beam.num_particles == beam_loaded.num_particles
-    assert torch.allclose(beam.particles, beam_loaded.particles)
-    assert torch.allclose(beam.particle_charges, beam_loaded.particle_charges)
+    assert original_cheetah_beam.num_particles == loaded_cheetah_beam.num_particles
+    assert torch.allclose(
+        original_cheetah_beam.particles, loaded_cheetah_beam.particles
+    )
+    assert torch.allclose(
+        original_cheetah_beam.particle_charges, loaded_cheetah_beam.particle_charges
+    )
 
 
-def test_particlebeam_to_and_from_openpmd_h5():
+def test_particlebeam_to_and_from_openpmd_h5(tmp_path):
+    """
+    Test that a `ParticleBeam` can be saved to an OpenPMD HDF5 file and loaded back,
+    checking that the loaded `ParticleBeam` is the same as the original.
+    """
+    reference_energy = torch.tensor(1e6)
 
-    ref_energy = torch.tensor(1e6)
-    beam = ParticleBeam.from_parameters(
-        num_particles=10000,
+    original_cheetah_beam = cheetah.ParticleBeam.from_parameters(
+        num_particles=10_000,
         mu_x=torch.tensor(1e-4),
         sigma_x=torch.tensor(2e-5),
         mu_y=torch.tensor(1e-4),
         sigma_y=torch.tensor(2e-5),
         sigma_p=torch.tensor(1e-4),
-        energy=ref_energy,
+        energy=reference_energy,
         total_charge=torch.tensor(1e-9),
         dtype=torch.float64,
     )
-
-    filename = "tests/resources/test_particlebeam_to_and_from_openpmd_h5.h5"
-    beam.save_as_openpmd_h5(filename)
-
-    # Test that the loaded particle group is the same as the original beam
-    beam_loaded = ParticleBeam.from_openpmd_file(
-        filename, energy=ref_energy, dtype=torch.float64
+    original_cheetah_beam.save_as_openpmd_h5(tmp_path / "particlegroup.h5")
+    loaded_cheetah_beam = cheetah.ParticleBeam.from_openpmd_file(
+        tmp_path / "particlegroup.h5", energy=reference_energy, dtype=torch.float64
     )
 
-    assert beam.num_particles == beam_loaded.num_particles
-    assert torch.allclose(beam.particles, beam_loaded.particles)
-    assert torch.allclose(beam.particle_charges, beam_loaded.particle_charges)
-
-    # Clean up
-    import os
-
-    os.remove(filename)
+    assert original_cheetah_beam.num_particles == loaded_cheetah_beam.num_particles
+    assert torch.allclose(
+        original_cheetah_beam.particles, loaded_cheetah_beam.particles
+    )
+    assert torch.allclose(
+        original_cheetah_beam.particle_charges, loaded_cheetah_beam.particle_charges
+    )
