c07527e501b4628f567f950bf53f7ebacf0eb1a9
Christian Hespe <christian.hespe@desy.de>
2025-02-06 14:28:29 +0100

    Fix screen reading conversion

--- cheetah/accelerator/screen.py
+++ cheetah/accelerator/screen.py
@@ -73,6 +73,9 @@ class Screen(Element):
         self.register_buffer("pixel_size", torch.tensor((1e-3, 1e-3), **factory_kwargs))
         self.register_buffer("misalignment", torch.tensor((0.0, 0.0), **factory_kwargs))
         self.register_buffer("kde_bandwidth", torch.clone(self.pixel_size[0]))
+        self.register_buffer(
+            "cached_reading", torch.full(resolution, torch.nan, **factory_kwargs)
+        )
 
         if pixel_size is not None:
             self.pixel_size = torch.as_tensor(pixel_size, **factory_kwargs)
@@ -82,7 +85,6 @@ class Screen(Element):
             self.kde_bandwidth = torch.as_tensor(kde_bandwidth, **factory_kwargs)
 
         self.set_read_beam(None)
-        self.cached_reading = None
 
     @property
     def is_skippable(self) -> bool:
@@ -190,8 +192,7 @@ class Screen(Element):
 
     @property
     def reading(self) -> torch.Tensor:
-        image = None
-        if self.cached_reading is not None:
+        if not torch.all(torch.isnan(self.cached_reading)):
             return self.cached_reading
 
         read_beam = self.get_read_beam()
@@ -296,7 +297,12 @@ class Screen(Element):
         # prevent `nn.Module` from intercepting the read beam, which is itself an
         # `nn.Module`, and registering it as a submodule of the screen.
         self._read_beam = value
-        self.cached_reading = None
+        self.cached_reading = torch.full(
+            self.resolution,
+            torch.nan,
+            device=self.cached_reading.device,
+            dtype=self.cached_reading.dtype,
+        )
 
     def split(self, resolution: torch.Tensor) -> list[Element]:
         return [self]
