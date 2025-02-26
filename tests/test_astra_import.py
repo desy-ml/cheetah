74236ce6efe3411de07103d253e33e95f1e218f4
Jan Kaiser <jan.kaiser.email@googlemail.com>
2025-02-26 16:24:47 +0100

    Test Astra beam import dtype test

--- tests/test_astra_import.py
+++ tests/test_astra_import.py
@@ -45,16 +45,18 @@ def test_astra_to_particle_beam():
 
 
 @pytest.mark.parametrize("BeamClass", [cheetah.ParameterBeam, cheetah.ParticleBeam])
-@pytest.mark.parametrize("target_dtype", [torch.float32, torch.float64, None])
-def test_astra_to_beam_dtypes(BeamClass: cheetah.Beam, target_dtype: torch.dtype):
-    """Test that Astra beams are correctly loaded into cheetah beams."""
+@pytest.mark.parametrize("target_dtype", [None, torch.float32, torch.float64])
+def test_dtypes(BeamClass: cheetah.Beam, target_dtype: torch.dtype):
+    """
+    Test that Astra beams are correctly loaded into different types of Cheetah beams
+    with different dtypes.
+    """
+    # If no target_dtype is specified, the default dtype of PyTorch should be used
+    target_dtype = target_dtype or torch.get_default_dtype()
+
     beam = BeamClass.from_astra(
         "tests/resources/ACHIP_EA1_2021.1351.001", dtype=target_dtype
     )
-
-    # The imported numpy data has dtype float64, therefore that should be the default
-    if target_dtype is None:
-        target_dtype = torch.float64
 
     assert beam.mu_x.dtype == target_dtype
     assert beam.mu_px.dtype == target_dtype
