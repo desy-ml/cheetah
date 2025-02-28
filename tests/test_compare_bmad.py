6310cb52a8c72426f82c224cd80503b214a6a15b
Jan Kaiser <jan.kaiser.email@googlemail.com>
2025-02-28 16:24:04 +0100

    Add a test that compares the cavity result to a direct run of Bmad

--- tests/test_compare_bmad.py
+++ tests/test_compare_bmad.py
@@ -155,3 +155,89 @@ def test_different_species_in_different_elements(
     )
 
     assert torch.allclose(outgoing_bmad_coordinates, x_tao, atol=1e-14)
+
+
+@pytest.mark.parametrize("cavity_type", ["standing_wave", "traveling_wave"])
+@pytest.mark.parametrize("phase", [0.0, 30.0])
+def test_cavity(tmp_path, cavity_type, phase):
+    """
+    Test that tracking a particle through a cavity in Cheetah agrees with Bmad results.
+    """
+    bmad_cavity_lattice_str = f"""
+    parameter[lattice] = test_cavity
+
+    beginning[beta_a] = 5.91253677 ! m a-mode beta function
+    beginning[beta_b] = 5.91253677 ! m b-mode beta function
+    beginning[alpha_a] = 3.55631308 ! a-mode alpha function
+    beginning[alpha_b] = 3.55631308 ! b-mode alpha function
+    beginning[p0c] = 6e6 ! eV    Or can set beginning[p0c]
+
+    parameter[geometry] = open  ! Or closed
+    parameter[particle] = electron  ! Reference particle.
+
+    c: lcavity, rf_frequency = 1.3e9, l = 1.0377, voltage = 0.01815975e9, phi0 = {phase}, cavity_type = {cavity_type}
+
+    lat: line = (c) ! List of lattice elements
+    use, lat ! Line used to construct the lattice
+    """
+    bmad_lattice_path = tmp_path / f"test_cavity_{cavity_type}_{phase}.bmad"
+    with open(bmad_lattice_path, "w") as f:
+        f.write(bmad_cavity_lattice_str)
+
+    tao = Tao(f"-lat {bmad_lattice_path} -noplot")
+
+    coordinate_list = [1e-3, 2e-3, -3e-3, -1e-3, 2e-3, -1e-3]
+    coordinates = torch.tensor(coordinate_list, dtype=torch.double)
+
+    p0c = torch.tensor(6e6, dtype=torch.double)
+    mc2 = cheetah.Species("electron").mass_eV
+
+    tau, delta, ref_energy = bmad_to_cheetah_z_pz(
+        coordinates[4], coordinates[5], p0c, mc2
+    )
+
+    cheetah_element = cheetah.Cavity(
+        length=torch.tensor(1.0377),
+        voltage=torch.tensor(0.01815975e9),
+        phase=torch.tensor(phase),
+        frequency=torch.tensor(1.3e9),
+        cavity_type=cavity_type,
+        dtype=torch.double,
+    )
+
+    cheetah_coordinates = torch.ones((1, 7), dtype=torch.double)
+    cheetah_coordinates[:, :4] = coordinates[:4]
+    cheetah_coordinates[:, 4] = tau
+    cheetah_coordinates[:, 5] = delta
+
+    incoming = cheetah.ParticleBeam(
+        particles=cheetah_coordinates,
+        energy=ref_energy,
+        species=cheetah.Species("electron"),
+        dtype=torch.double,
+    )
+
+    # Track with Cheetah
+    outgoing = cheetah_element(incoming)
+    # Convert to Bmad coordinates
+    outgoing_bmad_coordinates, _ = cheetah_to_bmad_coords(
+        outgoing.particles, ref_energy=outgoing.energy, mc2=outgoing.species.mass_eV
+    )
+
+    # Track with Tao
+    tao_set_particle_start(tao, coordinate_list)
+    orbit_out = tao.orbit_at_s(ele=1)
+
+    x_tao = torch.tensor(
+        [
+            orbit_out["x"],
+            orbit_out["px"],
+            orbit_out["y"],
+            orbit_out["py"],
+            orbit_out["z"],
+            orbit_out["pz"],
+        ],
+        dtype=torch.double,
+    )
+
+    assert torch.allclose(outgoing_bmad_coordinates, x_tao, atol=1e-14)
