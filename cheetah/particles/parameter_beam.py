8c7de4c119f23fe8c73b64351d036a0630a3ea0b
Jan Kaiser <jan.kaiser@desy.de>
2025-03-12 10:30:59 +0100

    Assume more reasonable default values for `total_charge` (#377)
    * Assume more reasonable default values for `total_charge`
    
    * Update changelog
    
    * Fix failing test
    
    ---------
    
    Co-authored-by: Chenran Xu <xuchenran@gmail.com>

--- cheetah/particles/parameter_beam.py
+++ cheetah/particles/parameter_beam.py
@@ -37,18 +37,21 @@ class ParameterBeam(Beam):
         factory_kwargs = {"device": device, "dtype": dtype}
         super().__init__()
 
+        self.species = species if species is not None else Species("electron")
+
         self.register_buffer("mu", None)
         self.register_buffer("cov", None)
         self.register_buffer("energy", None)
-        self.register_buffer("total_charge", torch.tensor(0.0, **factory_kwargs))
+        self.register_buffer(
+            "total_charge",
+            torch.as_tensor(self.species.charge_coulomb, **factory_kwargs),
+        )
 
         self.mu = torch.as_tensor(mu, **factory_kwargs)
         self.cov = torch.as_tensor(cov, **factory_kwargs)
         self.energy = torch.as_tensor(energy, **factory_kwargs)
         if total_charge is not None:
             self.total_charge = torch.as_tensor(total_charge, **factory_kwargs)
-
-        self.species = species if species is not None else Species("electron")
 
     @classmethod
     def from_parameters(
