fc2976ff4d78465d471f486b67e8be182a9142b3
Remi Lehe <remi.lehe@normalesup.org>
2025-04-08 08:48:37 +0200

    Change line length

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -31,8 +31,9 @@ def test_twiss_plot():
 
 def test_mean_and_std_particle_plot():
     """
-    Test that the mean and standard deviation particle plot does not raise an exception using the example
-    from the `simple.ipynb` example notebook from the documentation.
+    Test that the mean and standard deviation particle plot does not
+    raise an exception using the example from the `simple.ipynb`
+    example notebook from the documentation.
     """
     segment = cheetah.Segment(
         elements=[
