94d64c395d6c1a366f0643e3b8fdb4d4242c18a8
amy <amylizzlep@gmail.com>
2025-04-09 16:23:44 +0200

    forgot to run black lol

--- cheetah/converters/utils/rpn.py
+++ cheetah/converters/utils/rpn.py
@@ -1,5 +1,6 @@
 from typing import Any
 
+
 def try_eval_expression(expression: str, context: dict) -> Any:
     """
     Tries to evaluate an expression in reverse Polish notation.
@@ -9,30 +10,37 @@ def try_eval_expression(expression: str, context: dict) -> Any:
     stack = []
     stripped = expression.strip()
     for token in stripped.split(" "):
-        if token in '+-*/':
+        if token in "+-*/":
             b = stack.pop()
             a = stack.pop()
 
             # if else instead of match for compatibility reasons
-            if token == '+':
+            if token == "+":
                 stack.append(a + b)
-            elif token == '-':
+            elif token == "-":
                 stack.append(a - b)
-            elif token == '*':
+            elif token == "*":
                 stack.append(a * b)
-            elif token == '/':
+            elif token == "/":
                 stack.append(a / b)
             else:
-                raise SyntaxError(f"Invalid expression: {expression} - Invalid operator: {token}")
+                raise SyntaxError(
+                    f"Invalid expression: {expression} - Invalid operator: {token}"
+                )
         else:
             if token.isnumeric():
-                number = float(token) #putting everything as float since it's all torch in the back anyway
+                number = float(
+                    token
+                )  # putting everything as float since it's all torch in the back anyway
             elif token in context:
                 number = context[token]
             else:
-                raise SyntaxError(f"Invalid expression: {expression} - {token} is not a number or a variable")
+                raise SyntaxError(
+                    f"Invalid expression: {expression} - {token} is not a number or a variable"
+                )
             stack.append(number)
     if len(stack) != 1:
-        raise SyntaxError(f"Invalid expression: {expression} - Stack not empty after evaluation")
+        raise SyntaxError(
+            f"Invalid expression: {expression} - Stack not empty after evaluation"
+        )
     return stack[0]
-  
