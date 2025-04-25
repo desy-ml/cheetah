d27f71069826ff38c3dfcc66b50acc513ee6cce9
Jan Kaiser <jan.kaiser.email@googlemail.com>
2025-04-25 13:14:18 +0200

    Fix precedence that I ignored when originally combining the two steps from Ocelot

--- cheetah/track_methods.py
+++ cheetah/track_methods.py
@@ -156,15 +156,15 @@ def base_ttensor(
         *vector_shape, 1, 1, 1
     )
     T[..., 0, 0, 0] = -1 / 6 * khk * (sx**2 + dx) - 0.5 * hx * kx2 * sx**2
-    T[..., 0, 0, 1] = 2 * -1 / 6 * khk * sx * dx + 0.5 * hx * sx * cx
+    T[..., 0, 0, 1] = 2 * (-1 / 6 * khk * sx * dx + 0.5 * hx * sx * cx)
     T[..., 0, 1, 1] = -1 / 6 * khk * dx**2 + 0.5 * hx * dx * cx
-    T[..., 0, 0, 5] = (
-        2 * -hx / 12 / beta * khk * (3 * sx * j1 - dx**2)
+    T[..., 0, 0, 5] = 2 * (
+        -hx / 12 / beta * khk * (3 * sx * j1 - dx**2)
         + 0.5 * hx**2 / beta * sx**2
         + 0.25 / beta * k1 * length * sx
     )
-    T[..., 0, 1, 5] = (
-        2 * -hx / 12 / beta * khk * (sx * dx**2 - 2 * cx * j2)
+    T[..., 0, 1, 5] = 2 * (
+        -hx / 12 / beta * khk * (sx * dx**2 - 2 * cx * j2)
         + 0.25 * hx**2 / beta * (sx * dx + cx * j1)
         - 0.25 / beta * (sx + length * cx)
     )
@@ -175,17 +175,17 @@ def base_ttensor(
         - 0.5 * hx / (beta**2) * igamma2 * dx
     )
     T[..., 0, 2, 2] = k1 * k2 * jd + 0.5 * (k2 + hx * k1) * dx
-    T[..., 0, 2, 3] = 2 * 0.5 * k2 * js
+    T[..., 0, 2, 3] = 2 * (0.5 * k2 * js)
     T[..., 0, 3, 3] = k2 * jd - 0.5 * hx * dx
     T[..., 1, 0, 0] = -1 / 6 * khk * sx * (1 + 2 * cx)
-    T[..., 1, 0, 1] = 2 * -1 / 6 * khk * dx * (1 + 2 * cx)
+    T[..., 1, 0, 1] = 2 * (-1 / 6 * khk * dx * (1 + 2 * cx))
     T[..., 1, 1, 1] = -1 / 3 * khk * sx * dx - 0.5 * hx * sx
-    T[..., 1, 0, 5] = 2 * -hx / 12 / beta * khk * (
-        3 * cx * j1 + sx * dx
-    ) - 0.25 / beta * k1 * (sx - length * cx)
-    T[..., 1, 1, 5] = (
-        2 * -hx / 12 / beta * khk * (3 * sx * j1 + dx**2)
-        + 0.25 / beta * k1 * length * sx
+    T[..., 1, 0, 5] = 2 * (
+        -hx / 12 / beta * khk * (3 * cx * j1 + sx * dx)
+        - 0.25 / beta * k1 * (sx - length * cx)
+    )
+    T[..., 1, 1, 5] = 2 * (
+        -hx / 12 / beta * khk * (3 * sx * j1 + dx**2) + 0.25 / beta * k1 * length * sx
     )
     T[..., 1, 5, 5] = (
         -(hx**2) / 6 / beta**2 * khk * (sx * dx**2 - 2 * cx * j2)
@@ -193,80 +193,79 @@ def base_ttensor(
         - 0.5 * hx / beta**2 * igamma2 * sx
     )
     T[..., 1, 2, 2] = k1 * k2 * js + 0.5 * (k2 + hx * k1) * sx
-    T[..., 1, 2, 3] = 2 * 0.5 * k2 * jc
+    T[..., 1, 2, 3] = 2 * (0.5 * k2 * jc)
     T[..., 1, 3, 3] = k2 * js - 0.5 * hx * sx
-    T[..., 2, 0, 2] = (
-        2 * 0.5 * k2 * (cy * jc - 2 * k1 * sy * js) + 0.5 * hx * k1 * sx * sy
+    T[..., 2, 0, 2] = 2 * (
+        0.5 * k2 * (cy * jc - 2 * k1 * sy * js) + 0.5 * hx * k1 * sx * sy
     )
-    T[..., 2, 0, 3] = 2 * 0.5 * k2 * (sy * jc - 2 * cy * js) + 0.5 * hx * sx * cy
-    T[..., 2, 1, 2] = (
-        2 * 0.5 * k2 * (cy * js - 2 * k1 * sy * jd) + 0.5 * hx * k1 * dx * sy
+    T[..., 2, 0, 3] = 2 * (0.5 * k2 * (sy * jc - 2 * cy * js) + 0.5 * hx * sx * cy)
+    T[..., 2, 1, 2] = 2 * (
+        0.5 * k2 * (cy * js - 2 * k1 * sy * jd) + 0.5 * hx * k1 * dx * sy
     )
-    T[..., 2, 1, 3] = 2 * 0.5 * k2 * (sy * js - 2 * cy * jd) + 0.5 * hx * dx * cy
-    T[..., 2, 2, 5] = (
-        2 * 0.5 * hx / beta * k2 * (cy * jd - 2 * k1 * sy * jf)
+    T[..., 2, 1, 3] = 2 * (0.5 * k2 * (sy * js - 2 * cy * jd) + 0.5 * hx * dx * cy)
+    T[..., 2, 2, 5] = 2 * (
+        0.5 * hx / beta * k2 * (cy * jd - 2 * k1 * sy * jf)
         + 0.5 * hx**2 / beta * k1 * j1 * sy
         - 0.25 / beta * k1 * length * sy
     )
-    T[..., 2, 3, 5] = (
-        2 * 0.5 * hx / beta * k2 * (sy * jd - 2 * cy * jf)
+    T[..., 2, 3, 5] = 2 * (
+        0.5 * hx / beta * k2 * (sy * jd - 2 * cy * jf)
         + 0.5 * hx**2 / beta * j1 * cy
         - 0.25 / beta * (sy + length * cy)
     )
-    T[..., 3, 0, 2] = (
-        2 * 0.5 * k1 * k2 * (2 * cy * js - sy * jc) + 0.5 * (k2 + hx * k1) * sx * cy
+    T[..., 3, 0, 2] = 2 * (
+        0.5 * k1 * k2 * (2 * cy * js - sy * jc) + 0.5 * (k2 + hx * k1) * sx * cy
     )
-    T[..., 3, 0, 3] = (
-        2 * 0.5 * k2 * (2 * k1 * sy * js - cy * jc) + 0.5 * (k2 + hx * k1) * sx * sy
+    T[..., 3, 0, 3] = 2 * (
+        0.5 * k2 * (2 * k1 * sy * js - cy * jc) + 0.5 * (k2 + hx * k1) * sx * sy
     )
-    T[..., 3, 1, 2] = (
-        2 * 0.5 * k1 * k2 * (2 * cy * jd - sy * js) + 0.5 * (k2 + hx * k1) * dx * cy
+    T[..., 3, 1, 2] = 2 * (
+        0.5 * k1 * k2 * (2 * cy * jd - sy * js) + 0.5 * (k2 + hx * k1) * dx * cy
     )
-    T[..., 3, 1, 3] = (
-        2 * 0.5 * k2 * (2 * k1 * sy * jd - cy * js) + 0.5 * (k2 + hx * k1) * dx * sy
+    T[..., 3, 1, 3] = 2 * (
+        0.5 * k2 * (2 * k1 * sy * jd - cy * js) + 0.5 * (k2 + hx * k1) * dx * sy
     )
-    T[..., 3, 2, 5] = (
-        2 * 0.5 * hx / beta * k1 * k2 * (2 * cy * jf - sy * jd)
+    T[..., 3, 2, 5] = 2 * (
+        0.5 * hx / beta * k1 * k2 * (2 * cy * jf - sy * jd)
         + 0.5 * hx / beta * (k2 + hx * k1) * j1 * cy
         + 0.25 / beta * k1 * (sy - length * cy)
     )
-    T[..., 3, 3, 5] = (
-        2 * 0.5 * hx / beta * k2 * (2 * k1 * sy * jf - cy * jd)
+    T[..., 3, 3, 5] = 2 * (
+        0.5 * hx / beta * k2 * (2 * k1 * sy * jf - cy * jd)
         + 0.5 * hx / beta * (k2 + hx * k1) * j1 * sy
         - 0.25 / beta * k1 * length * sy
     )
-    T[..., 4, 0, 0] = -1 * hx / 12 / beta * khk * (
-        sx * dx + 3 * j1
-    ) - 0.25 / beta * k1 * (length - sx * cx)
-    T[..., 4, 0, 1] = -2 * hx / 12 / beta * khk * dx**2 + 0.25 / beta * k1 * sx**2
-    T[..., 4, 1, 1] = (
-        -1 * hx / 6 / beta * khk * j2
-        - 0.5 / beta * sx
-        - 0.25 / beta * k1 * (j1 - sx * dx)
+    T[..., 4, 0, 0] = -1 * (
+        hx / 12 / beta * khk * (sx * dx + 3 * j1)
+        - 0.25 / beta * k1 * (length - sx * cx)
     )
-    T[..., 4, 0, 5] = (
-        -2 * hx**2 / 12 / beta**2 * khk * (3 * dx * j1 - 4 * j2)
+    T[..., 4, 0, 1] = -2 * (hx / 12 / beta * khk * dx**2 + 0.25 / beta * k1 * sx**2)
+    T[..., 4, 1, 1] = -1 * (
+        hx / 6 / beta * khk * j2 - 0.5 / beta * sx - 0.25 / beta * k1 * (j1 - sx * dx)
+    )
+    T[..., 4, 0, 5] = -2 * (
+        hx**2 / 12 / beta**2 * khk * (3 * dx * j1 - 4 * j2)
         + 0.25 * hx / beta**2 * k1 * j1 * (1 + cx)
         + 0.5 * hx / beta**2 * igamma2 * sx
     )
-    T[..., 4, 1, 5] = (
-        -2 * hx**2 / 12 / beta**2 * khk * (dx * dx**2 - 2 * sx * j2)
+    T[..., 4, 1, 5] = -2 * (
+        hx**2 / 12 / beta**2 * khk * (dx * dx**2 - 2 * sx * j2)
         + 0.25 * hx / beta**2 * k1 * sx * j1
         + 0.5 * hx / beta**2 * igamma2 * dx
     )
-    T[..., 4, 5, 5] = (
-        -1 * hx**3 / 6 / beta**3 * khk * (3 * j3 - 2 * dx * j2)
+    T[..., 4, 5, 5] = -1 * (
+        hx**3 / 6 / beta**3 * khk * (3 * j3 - 2 * dx * j2)
         + hx**2 / 6 / beta**3 * k1 * (sx * dx**2 - j2 * (1 + 2 * cx))
         + 1.5 / beta**3 * igamma2 * (hx**2 * j1 - length)
     )
-    T[..., 4, 2, 2] = (
-        -1 * -hx / beta * k1 * k2 * jf
+    T[..., 4, 2, 2] = -1 * (
+        -hx / beta * k1 * k2 * jf
         - 0.5 * hx / beta * (k2 + hx * k1) * j1
         + 0.25 / beta * k1 * (length - cy * sy)
     )
-    T[..., 4, 2, 3] = -2 * -0.5 * hx / beta * k2 * jd - 0.25 / beta * k1 * sy**2
-    T[..., 4, 3, 3] = (
-        -1 * -hx / beta * k2 * jf
+    T[..., 4, 2, 3] = -2 * (-0.5 * hx / beta * k2 * jd - 0.25 / beta * k1 * sy**2)
+    T[..., 4, 3, 3] = -1 * (
+        -hx / beta * k2 * jf
         + 0.5 * hx**2 / beta * j1
         - 0.25 / beta * (length + cy * sy)
     )
