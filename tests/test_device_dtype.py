69be86f9a0493819a779e08ccc2f3eb64f313778
Christian Hespe <christian.hespe@desy.de>
2025-04-28 16:07:56 +0200

    Make the dtype tests apply to more element and beam classes

--- tests/test_device_dtype.py
+++ tests/test_device_dtype.py
@@ -1,3 +1,5 @@
+import inspect
+
 import pytest
 import torch
 
@@ -22,26 +24,19 @@ from cheetah.utils import is_mps_available_and_functional
         ),
     ],
 )
-def test_move_quadrupole_to_device(target_device: torch.device):
-    """Test that a quadrupole magnet can be successfully moved to a different device."""
-    quad = cheetah.Quadrupole(
-        length=torch.tensor(0.2), k1=torch.tensor(4.2), name="my_quad"
-    )
+def test_move_element_to_device(mwe_cheetah_element, target_device: torch.device):
+    """Test that elements can be successfully moved to a different device."""
 
-    # Test that by default the quadrupole is on the CPU
-    assert quad.length.device.type == "cpu"
-    assert quad.k1.device.type == "cpu"
-    assert quad.misalignment.device.type == "cpu"
-    assert quad.tilt.device.type == "cpu"
+    # Test that by default the element is on the CPU
+    for buffer in mwe_cheetah_element.buffers():
+        assert buffer.dtype == "cpu"
 
-    # Move the quadrupole to the target device
-    quad.to(target_device)
+    # Move the element to the target device
+    mwe_cheetah_element.to(target_device)
 
-    # Test that the quadrupole is now on the target device
-    assert quad.length.device.type == target_device.type
-    assert quad.k1.device.type == target_device.type
-    assert quad.misalignment.device.type == target_device.type
-    assert quad.tilt.device.type == target_device.type
+    # Test that the element is now on the target device
+    for buffer in mwe_cheetah_element.buffers():
+        assert buffer.dtype == target_device.type
 
 
 @pytest.mark.skip_element([cheetah.Marker, cheetah.Segment])
@@ -49,9 +44,12 @@ def test_forced_element_dtype(mwe_cheetah_element):
     """
     Test that the dtype is properly overridden for all element classes.
     """
-    double_element = mwe_cheetah_element.double()
+    # Ensure everything is float32 by default
+    for buffer in mwe_cheetah_element.buffers():
+        assert buffer.dtype == torch.float32
 
     # Create new element with overriden dtype
+    double_element = mwe_cheetah_element.double()
     half_element = double_element.__class__(
         **{
             feature: getattr(double_element, feature)
@@ -69,9 +67,12 @@ def test_infer_element_dtype(mwe_cheetah_element):
     """
     Test that the dtype is properly inferred for all element classes.
     """
-    double_element = mwe_cheetah_element.double()
+    # Ensure everything is float32 by default
+    for buffer in mwe_cheetah_element.buffers():
+        assert buffer.dtype == torch.float32
 
     # Create new element and infer the dtype from passed buffers
+    double_element = mwe_cheetah_element.double()
     inferred_element = double_element.__class__(
         **{
             feature: getattr(double_element, feature)
@@ -83,51 +84,71 @@ def test_infer_element_dtype(mwe_cheetah_element):
         assert buffer.dtype == torch.float64
 
 
-def test_conflicting_quadrupole_dtype():
-    """
-    Test that creating a quadrupole with conflicting argument dtypes fails.
-    """
-    with pytest.raises(AssertionError):
-        cheetah.Quadrupole(
-            length=torch.tensor(1.0, dtype=torch.float32),
-            k1=torch.tensor(10.0, dtype=torch.float64),
+def test_conflicting_element_dtype(mwe_cheetah_element):
+    """Test that creating elements with conflicting argument dtypes fails."""
+    arguments = inspect.signature(mwe_cheetah_element.__init__).parameters
+
+    # Extract required arguments for this element class
+    required_arguments = {
+        name: getattr(mwe_cheetah_element, name)
+        for name, properties in arguments.items()
+        if name != "self" and properties.default is inspect._empty
+    }
+
+    # Generate list of optional tensor arguments whose dtype can be varied
+    optional_tensor_arguments = {
+        feature: getattr(mwe_cheetah_element, feature)
+        for feature in mwe_cheetah_element.defining_features
+        if isinstance(getattr(mwe_cheetah_element, feature), torch.Tensor)
+        and feature not in required_arguments
+    }
+
+    # Ensure that at least one tensor is part of the arguments that are passed each call
+    if (
+        not any(
+            isinstance(value, torch.Tensor) for value in required_arguments.values()
+        )
+        and len(optional_tensor_arguments) > 0
+    ):
+        name, value = optional_tensor_arguments.popitem()
+        required_arguments[name] = value
+
+    # Vary individual optional arguments dtype
+    for name, value in optional_tensor_arguments.items():
+        with pytest.raises(AssertionError):
+            # Contains conflicting dtype
+            mwe_cheetah_element.__class__(
+                **{name: value.double()}, **required_arguments
+            )
+
+        # Conflict can be overriden by manual dtype selection
+        mwe_cheetah_element.__class__(
+            **{name: value.double()}, **required_arguments, dtype=torch.float16
         )
 
-    # Ensure that the conflict can be solved by explicit dtype selection
-    quad = cheetah.Quadrupole(
-        length=torch.tensor(1.0, dtype=torch.float32),
-        k1=torch.tensor(10.0, dtype=torch.float64),
-        dtype=torch.float16,
-    )
-    assert quad.length.dtype == torch.float16
+
+def test_change_element_dtype(mwe_cheetah_element):
+    """Test that elements can be successfully changed to a different dtype."""
+
+    # Test that by default the element is of dtype float32
+    for buffer in mwe_cheetah_element.buffers():
+        assert buffer.dtype == torch.float32
+
+    # Change the dtype of the element
+    mwe_cheetah_element.to(torch.float64)
+
+    # Test that the element is now of dtype float64
+    for buffer in mwe_cheetah_element.buffers():
+        assert buffer.dtype == torch.float64
 
 
-def test_change_quadrupole_dtype():
-    """
-    Test that a quadrupole magnet can be successfully changed to a different dtype.
-    """
-    quad = cheetah.Quadrupole(
-        length=torch.tensor(0.2),
-        k1=torch.tensor(4.2),
-        name="my_quad",
-    )
-
-    # Test that by default the quadrupole is of dtype float32
-    assert quad.length.dtype == torch.float32
-    assert quad.k1.dtype == torch.float32
-    assert quad.misalignment.dtype == torch.float32
-    assert quad.tilt.dtype == torch.float32
-
-    # Change the dtype of the quadrupole
-    quad.to(torch.float64)
-
-    # Test that the quadrupole is now of dtype float64
-    assert quad.length.dtype == torch.float64
-    assert quad.k1.dtype == torch.float64
-    assert quad.misalignment.dtype == torch.float64
-    assert quad.tilt.dtype == torch.float64
-
-
+@pytest.mark.parametrize(
+    "BeamClass",
+    [
+        cheetah.ParameterBeam,
+        cheetah.ParticleBeam,
+    ],
+)
 @pytest.mark.parametrize(
     "target_device",
     [
@@ -145,21 +166,19 @@ def test_change_quadrupole_dtype():
         ),
     ],
 )
-def test_move_particlebeam_to_device(target_device: torch.device):
+def test_move_beam_to_device(BeamClass, target_device: torch.device):
     """Test that a particle beam can be successfully moved to a different device."""
-    beam = cheetah.ParticleBeam.from_parameters(num_particles=100_000)
+    beam = BeamClass.from_parameters()
 
-    # Test that by default the particle beam is on the CPU
-    assert beam.particles.device.type == "cpu"
-    assert beam.energy.device.type == "cpu"
-    assert beam.total_charge.device.type == "cpu"
+    # Test that by default the beam is on the CPU
+    for buffer in beam.buffers():
+        assert buffer.device.type == "cpu"
 
     beam.to(target_device)
 
-    # Test that the particle beam is now on the target device
-    assert beam.particles.device.type == target_device.type
-    assert beam.energy.device.type == target_device.type
-    assert beam.total_charge.device.type == target_device.type
+    # Test that the beam is now on the target device
+    for buffer in beam.buffers():
+        assert buffer.device.type == target_device.type
 
 
 @pytest.mark.parametrize(
@@ -211,34 +230,37 @@ def test_infer_beam_dtype(BeamClass):
         assert buffer.dtype == torch.float64
 
 
-def test_conflicting_particlebeam_dtype():
-    """
-    Test if creating a ParticleBeam with conflicting argument dtypes fails.
-    """
+@pytest.mark.parametrize(
+    "BeamClass",
+    [
+        cheetah.ParameterBeam,
+        cheetah.ParticleBeam,
+    ],
+)
+def test_conflicting_beam_dtype(BeamClass):
+    """Test if creating a beam with conflicting argument dtypes fails."""
     with pytest.raises(AssertionError):
-        cheetah.ParticleBeam.from_twiss(
+        BeamClass.from_twiss(
             beta_x=torch.tensor(1.0, dtype=torch.float32),
             beta_y=torch.tensor(2.0, dtype=torch.float64),
         )
 
 
-def test_change_particlebeam_dtype():
-    """
-    Test that a particle beam can be successfully changed to a different dtype.
-    """
-    beam = cheetah.ParticleBeam.from_parameters(num_particles=100_000)
+@pytest.mark.parametrize(
+    "BeamClass",
+    [
+        cheetah.ParameterBeam,
+        cheetah.ParticleBeam,
+    ],
+)
+def test_change_beam_dtype(BeamClass):
+    """Test that beams can be successfully changed to a different dtype."""
+    beam = BeamClass.from_parameters(dtype=torch.float64)
+    beam.to(torch.float16)
 
-    # Test that by default the particle beam is of dtype float32
-    assert beam.particles.dtype == torch.float32
-    assert beam.energy.dtype == torch.float32
-    assert beam.total_charge.dtype == torch.float32
-
-    beam.to(torch.float64)
-
-    # Test that the particle beam is now of dtype float64
-    assert beam.particles.dtype == torch.float64
-    assert beam.energy.dtype == torch.float64
-    assert beam.total_charge.dtype == torch.float64
+    # Test that the beam is now of dtype float16
+    for buffer in beam.buffers():
+        assert buffer.dtype == torch.float16
 
 
 @pytest.mark.parametrize(
@@ -256,10 +278,12 @@ def test_transformed_beam_dtype(BeamClass):
 
     # Verify the dtype is kept by default
     transformed_beam = beam.transformed_to(mu_x=torch.tensor(-2e-5))
-    assert transformed_beam.mu_x.dtype == torch.float64
+    for buffer in transformed_beam.buffers():
+        assert buffer.dtype == torch.float64
 
     # Check that the manual dtype selection works
     transformed_beam = beam.transformed_to(
         mu_x=torch.tensor(-2e-5), dtype=torch.float32
     )
-    assert transformed_beam.mu_x.dtype == torch.float32
+    for buffer in transformed_beam.buffers():
+        assert buffer.dtype == torch.float32
