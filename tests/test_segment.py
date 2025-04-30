0b72824bae9d2071eb535291e9ab02fab64de45b
Jan Kaiser <jan.kaiser@desy.de>
2025-04-30 13:36:23 +0200

    Apply suggestions from code review

--- tests/test_segment.py
+++ tests/test_segment.py
@@ -52,8 +52,8 @@ def test_subcell_endpoint():
     assert len(segment.subcell("drift_2", include_end=False).elements) == 8
 
 
-@pytest.mark.parametrize("recursive", [True, False])
-def test_attr_setting_by_element_type_convenience_method(recursive):
+@pytest.mark.parametrize("is_recursive", [True, False])
+def test_attr_setting_by_element_type_convenience_method(is_recursive):
     """
     Test that the convenience method for setting attributes by element type works as
     expected.
@@ -70,7 +70,7 @@ def test_attr_setting_by_element_type_convenience_method(recursive):
     )
 
     segment.set_attrs_on_every_element_of_type(
-        cheetah.Drift, is_recursive=recursive, length=torch.tensor(4.2)
+        cheetah.Drift, is_recursive=is_recursive, length=torch.tensor(4.2)
     )
 
     for element in segment.elements:
@@ -78,7 +78,7 @@ def test_attr_setting_by_element_type_convenience_method(recursive):
             assert element.length == 4.2
         elif isinstance(element, cheetah.Segment):
             for subelement in element.elements:
-                assert subelement.length == 4.2 if recursive else 0.5
+                assert subelement.length == 4.2 if is_recursive else 0.5
         elif isinstance(element, cheetah.Quadrupole):
             assert element.length == 0.6
         else:
