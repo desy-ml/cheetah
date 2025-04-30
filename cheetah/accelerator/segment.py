6ec84676b7fb901fd88787ba8c092309b1907bd6
Jan Kaiser <jan.kaiser.email@googlemail.com>
2025-04-30 11:55:37 +0200

    Slighty change function name

--- cheetah/accelerator/segment.py
+++ cheetah/accelerator/segment.py
@@ -448,14 +448,14 @@ class Segment(Element):
             for split_element in element.split(resolution)
         ]
 
-    def set_attrs_of_every_element_of_type(
+    def set_attrs_on_every_element_of_type(
         self,
         element_type: type[Element] | tuple[type[Element]],
         is_recursive: bool = True,
         **kwargs: dict[str, Any],
     ) -> None:
         """
-        Set attributes of every element of a specific type in the segment.
+        Set attributes on every element of a specific type in the segment.
 
         :param element_type: Type of the elements to set the attributes for.
         :param is_recursive: If `True`, the this method is applied to nested `Segment`s
@@ -468,7 +468,9 @@ class Segment(Element):
                 for key, value in kwargs.items():
                     setattr(element, key, value)
             elif is_recursive and isinstance(element, Segment):
-                element.set_attrs_of_every_element_of_type(element_type, is_recursive=True, **kwargs)
+                element.set_attrs_on_every_element_of_type(
+                    element_type, is_recursive=True, **kwargs
+                )
 
     def plot(self, ax: plt.Axes, s: float, vector_idx: tuple | None = None) -> None:
         element_lengths = [element.length for element in self.elements]
