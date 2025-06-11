df200a88daefb8f10ec468b2304888edee5841b3
Christian Hespe <christian.hespe@desy.de>
2025-06-11 12:13:41 +0200

    Make dtype conversion test of Elements not depend on buffer registration

--- tests/test_elements.py
+++ tests/test_elements.py
@@ -1,4 +1,5 @@
 import pytest
+import torch
 
 
 @pytest.mark.for_every_mwe_element("mwe_element")
@@ -18,3 +19,26 @@ def test_all_element_subclasses_is_skippable_boolean(mwe_element):
     Test that the `is_skippable` property of all `Element` subclasses returns a boolean.
     """
     assert isinstance(mwe_element.is_skippable, bool)
+
+
+@pytest.mark.for_every_mwe_element("mwe_element")
+def test_defining_features_dtype(mwe_element):
+    """
+    Test that all defining features of `Element` subclasses that are `torch.Tensor`are
+    properly converted between different dtypes. This transitively tests if all defining
+    features are registered as pytorch buffers.
+    """
+
+    # Ensure all features have the same dtype initially
+    for feature in mwe_element.defining_features:
+        value = getattr(mwe_element, feature)
+        if isinstance(value, torch.Tensor):
+            assert value.dtype == torch.float32
+
+    mwe_element.to(torch.float64)
+
+    # Ensure all features have been converted to float64
+    for feature in mwe_element.defining_features:
+        value = getattr(mwe_element, feature)
+        if isinstance(value, torch.Tensor):
+            assert value.dtype == torch.float64
