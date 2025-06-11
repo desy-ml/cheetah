84c6dad59f55ebbe06ad5512c150ff453aae21ce
Jan Kaiser <jan.kaiser.email@googlemail.com>
2025-06-11 13:36:18 +0200

    Fix some import failure in tests

--- cheetah/utils/__init__.py
+++ cheetah/utils/__init__.py
@@ -3,7 +3,6 @@ from .argument_verification import verify_device_and_dtype  # noqa: F401
 from .device import is_mps_available_and_functional  # noqa: F401
 from .elementwise_linspace import elementwise_linspace  # noqa: F401
 from .kde import kde_histogram_1d, kde_histogram_2d  # noqa: F401
-from .names import UniqueNameGenerator, is_name_clean, sanitize_name  # noqa: F401
 from .physics import compute_relativistic_factors  # noqa: F401
 from .plot import (  # noqa: F401
     format_axis_as_percentage,
@@ -14,5 +13,6 @@ from .statistics import (  # noqa: F401
     unbiased_weighted_std,
     unbiased_weighted_variance,
 )
+from .unique_name_generator import UniqueNameGenerator  # noqa: F401
 from .vector import squash_index_for_unavailable_dims  # noqa: F401
 from .warnings import DirtyNameWarning, PhysicsWarning  # noqa: F401
