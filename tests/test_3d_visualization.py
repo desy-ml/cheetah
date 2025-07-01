b07a349ed7abe5295a39a10c43ce25ae0a0f29e8
Jan Kaiser <jan.kaiser.email@googlemail.com>
2025-07-01 18:31:29 +0200

    Initial cleanup of visualisation tests

--- tests/test_3d_visualization.py
+++ tests/test_3d_visualization.py
@@ -1,30 +1,28 @@
-# tests/test_segment_3d_builder.py
 import os
 
 import pytest
 
-from cheetah import Segment
-from cheetah.utils.segment_3d_builder import Segment3DBuilder
+import cheetah
 
 
 @pytest.fixture
 def segment():
     # Mock or create a Segment instance to be used in tests
-    return Segment.from_lattice_json(filepath="tests/mock_config.json")
+    return cheetah.Segment.from_lattice_json(filepath="tests/mock_config.json")
 
 
 def test_builder_initialization(segment):
-    builder = Segment3DBuilder(segment)
+    builder = cheetah.utils.segment_3d_builder.Segment3DBuilder(segment)
     assert builder.segment == segment
 
 
 def test_component_positions(segment):
-    builder = Segment3DBuilder(segment)
+    builder = cheetah.utils.segment_3d_builder.Segment3DBuilder(segment)
     assert isinstance(builder.component_positions, dict)
 
 
 def test_export_function(segment):
-    builder = Segment3DBuilder(segment)
+    builder = cheetah.utils.segment_3d_builder.Segment3DBuilder(segment)
     output_filename = "test_scene.glb"
     builder.build_segment(output_filename=output_filename, is_export_enabled=True)
     assert os.path.exists(output_filename)
