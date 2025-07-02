80f7a9f48a5bd6361c81f02a345b1cdcd53ef9dc
Christian Hespe <christian.hespe@desy.de>
2025-07-02 14:00:21 +0200

    Extend basic element tracking test to ParameterBeam

--- tests/test_elements.py
+++ tests/test_elements.py
@@ -64,7 +64,7 @@ def test_defining_features_dtype(element):
     ],
     ids=["cpu-float32", "cpu-float64", "cuda-float32", "mps-float32"],
 )
-def test_tracking_with_device_and_dtype(element, device, dtype):
+def test_particle_beam_tracking_with_device_and_dtype(element, device, dtype):
     """
     Test that element subclasses work correctly on various devices and with various
     dtypes if tracked with a `ParticleBeam`
@@ -78,6 +78,69 @@ def test_tracking_with_device_and_dtype(element, device, dtype):
     ).to(device=device, dtype=dtype)
     incoming_beam = cheetah.ParticleBeam.from_parameters(
         num_particles=10_000,
+        total_charge=torch.tensor(1e-9),
+        mu_x=torch.tensor(5e-5),
+        sigma_px=torch.tensor(1e-4),
+        sigma_py=torch.tensor(1e-4),
+    ).to(device=device, dtype=dtype)
+
+    # Run in part to see if errors are raised
+    outgoing_beam = segment.track(incoming_beam)
+
+    # Check device and dtype of the output
+    for attribute in outgoing_beam.UNVECTORIZED_NUM_ATTR_DIMS.keys():
+        assert getattr(outgoing_beam, attribute).device.type == device.type
+        assert getattr(outgoing_beam, attribute).dtype == dtype
+
+
+@pytest.mark.for_every_element(
+    "element",
+    xfail_if=lambda element: isinstance(
+        element, (cheetah.SpaceChargeKick, cheetah.TransverseDeflectingCavity)
+    )
+    or (isinstance(element, cheetah.Aperture) and element.is_active)
+    or (
+        isinstance(
+            element, (cheetah.Dipole, cheetah.Drift, cheetah.Quadrupole, cheetah.RBend)
+        )
+        and element.tracking_method == "bmadx"
+    ),
+)
+@pytest.mark.parametrize(
+    "device, dtype",
+    [
+        (torch.device("cpu"), torch.float32),
+        (torch.device("cpu"), torch.float64),
+        pytest.param(
+            torch.device("cuda"),
+            torch.float32,
+            marks=pytest.mark.skipif(
+                not torch.cuda.is_available(), reason="CUDA not available"
+            ),
+        ),
+        pytest.param(
+            torch.device("mps"),
+            torch.float32,
+            marks=pytest.mark.skipif(
+                not is_mps_available_and_functional(), reason="MPS not available"
+            ),
+        ),
+    ],
+    ids=["cpu-float32", "cpu-float64", "cuda-float32", "mps-float32"],
+)
+def test_parameter_beam_tracking_with_device_and_dtype(element, device, dtype):
+    """
+    Test that element subclasses work correctly on various devices and with various
+    dtypes if tracked with a `ParticleBeam`
+    """
+    segment = cheetah.Segment(
+        elements=[
+            cheetah.Drift(length=torch.tensor(0.25)),
+            element,
+            cheetah.Drift(length=torch.tensor(0.25)),
+        ]
+    ).to(device=device, dtype=dtype)
+    incoming_beam = cheetah.ParameterBeam.from_parameters(
         total_charge=torch.tensor(1e-9),
         mu_x=torch.tensor(5e-5),
         sigma_px=torch.tensor(1e-4),
