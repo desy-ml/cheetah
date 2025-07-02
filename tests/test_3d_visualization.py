0ef06c7228b109e084eb250ab7627d1b4161c1cc
Jan Kaiser <jan.kaiser.email@googlemail.com>
2025-07-02 17:36:08 +0200

    Remove old `Segment3DBuilder` files and assosciated ones that are no longer needed

--- tests/test_3d_visualization.py
+++ tests/test_3d_visualization.py
@@ -1,33 +1,7 @@
-import os
-
-import pytest
 import torch
 import trimesh
 
 import cheetah
-
-
-@pytest.fixture
-def segment():
-    # Mock or create a Segment instance to be used in tests
-    return cheetah.Segment.from_lattice_json(filepath="tests/mock_config.json")
-
-
-def test_builder_initialization(segment):
-    builder = cheetah.utils.segment_3d_builder.Segment3DBuilder(segment)
-    assert builder.segment == segment
-
-
-def test_component_positions(segment):
-    builder = cheetah.utils.segment_3d_builder.Segment3DBuilder(segment)
-    assert isinstance(builder.component_positions, dict)
-
-
-def test_export_function(segment):
-    builder = cheetah.utils.segment_3d_builder.Segment3DBuilder(segment)
-    output_filename = "test_scene.glb"
-    builder.build_segment(output_filename=output_filename, is_export_enabled=True)
-    assert os.path.exists(output_filename)
 
 
 def test_segment_method():
@@ -48,4 +22,4 @@ def test_segment_method():
     )
     mesh = segment.to_mesh(s=0.0)
 
-    assert isinstance(mesh, trimesh.Scene)
+    assert isinstance(mesh, trimesh.Trimesh)
