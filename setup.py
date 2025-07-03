30ae249bebc31207b8332991ba530467a8804676
Jan Kaiser <jan.kaiser.email@googlemail.com>
2025-07-03 17:32:54 +0200

    Add `tqdm` dependency

--- setup.py
+++ setup.py
@@ -38,6 +38,6 @@ setup(
     ],
     extras_require={
         "openpmd": ["openpmd-beamphysics"],
-        "3d-visualization": ["trimesh>=4.0.0", "requests>=2.32.2"],
+        "3d-visualization": ["trimesh>=4.0.0", "requests>=2.32.2", "tqdm>=4.66.0"],
     },
 )
