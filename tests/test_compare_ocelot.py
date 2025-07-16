c16425066a6571920818484c9d44d10a51dedbec
Chenran Xu <chenran.xu@anl.gov>
2025-07-16 09:57:29 -0500

    Fix a SyntaxError in ocelot test

--- tests/test_compare_ocelot.py
+++ tests/test_compare_ocelot.py
@@ -33,7 +33,11 @@ def test_dipole(tracking_method):
         "tests/resources/ACHIP_EA1_2021.1351.001"
     )
     ocelot_bend = ocelot.Bend(l=0.1, angle=0.1)
-    ocelot_method = {"global" ocelot.SecondTM if tracking_method == "second_order" else ocelot.TransferMap}
+    ocelot_method = {
+        "global": (
+            ocelot.SecondTM if tracking_method == "second_order" else ocelot.TransferMap
+        )
+    }
 
     lattice = ocelot.MagneticLattice([ocelot_bend], method=ocelot_method)
     navigator = ocelot.Navigator(lattice)
