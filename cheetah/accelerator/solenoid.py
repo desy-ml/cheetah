Fix broadcasting in Solenoid

cheetah/accelerator/solenoid.py
@@ -62,20 +62,18 @@ class Solenoid(Element):
         factory_kwargs = {"device": self.length.device, "dtype": self.length.dtype}
 
         gamma, _, _ = compute_relativistic_factors(energy, species.mass_eV)
-        c = (self.length * self.k).cos()
-        s = (self.length * self.k).sin()
+        length, k, gamma = torch.broadcast_tensors(self.length, self.k, gamma)
 
-        s_k = torch.where(self.k == 0.0, self.length, s / self.k)
+        c = (length * k).cos()
+        s = (length * k).sin()
 
-        vector_shape = torch.broadcast_shapes(
-            self.length.shape, self.k.shape, energy.shape
-        )
+        s_k = torch.where(k == 0.0, length, s / k)
 
         r56 = torch.where(
-            gamma != 0, self.length / (1 - gamma.square()), self.length.new_zeros(())
+            gamma != 0, length / (1 - gamma.square()), length.new_zeros(())
         )
 
-        R = torch.eye(7, **factory_kwargs).expand((*vector_shape, 7, 7)).clone()
+        R = torch.eye(7, **factory_kwargs).expand((*length.shape, 7, 7)).clone()
         R[
             ...,
             (0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4),
@@ -86,17 +84,17 @@ class Solenoid(Element):
                 c * s_k,
                 s * c,
                 s * s_k,
-                -self.k * s * c,
+                -k * s * c,
                 c.square(),
-                -self.k * s.square(),
+                -k * s.square(),
                 s * c,
                 -s * c,
                 -s * s_k,
                 c.square(),
                 c * s_k,
-                self.k * s.square(),
+                k * s.square(),
                 -s * c,
-                -self.k * s * c,
+                -k * s * c,
                 c.square(),
                 r56,
             ],