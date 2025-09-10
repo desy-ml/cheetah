0b698835661bfdbd49e2f2d05120b1a9d150780d
Jan Kaiser <jan.kaiser.email@googlemail.com>
2025-09-10 16:12:33 +0200

    Apply faster indexing changes copied from #538

--- cheetah/accelerator/solenoid.py
+++ cheetah/accelerator/solenoid.py
@@ -62,37 +62,42 @@ class Solenoid(Element):
         factory_kwargs = {"device": self.length.device, "dtype": self.length.dtype}
 
         gamma, _, _ = compute_relativistic_factors(energy, species.mass_eV)
-        c = torch.cos(self.length * self.k)
-        s = torch.sin(self.length * self.k)
+        length, k, gamma = torch.broadcast_tensors(self.length, self.k, gamma)
 
-        s_k = torch.where(self.k == 0.0, self.length, s / self.k)
+        c = torch.cos(length * k)
+        s = torch.sin(length * k)
 
-        vector_shape = torch.broadcast_shapes(
-            self.length.shape, self.k.shape, energy.shape
+        s_k = torch.where(k == 0.0, length, s / k)
+
+        r56 = torch.where(gamma != 0, length / (1 - gamma**2), torch.zeros_like(length))
+
+        R = torch.eye(7, **factory_kwargs).repeat((*length.shape, 1, 1))
+        R[
+            ...,
+            (0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4),
+            (0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 5),
+        ] = torch.stack(
+            [
+                c**2,
+                c * s_k,
+                s * c,
+                s * s_k,
+                -k * s * c,
+                c**2,
+                -k * s**2,
+                s * c,
+                -s * c,
+                -s * s_k,
+                c**2,
+                c * s_k,
+                k * s**2,
+                -s * c,
+                -k * s * c,
+                c**2,
+                r56,
+            ],
+            dim=-1,
         )
-
-        r56 = torch.where(
-            gamma != 0, self.length / (1 - gamma**2), torch.zeros_like(self.length)
-        )
-
-        R = torch.eye(7, **factory_kwargs).repeat((*vector_shape, 1, 1))
-        R[..., 0, 0] = c**2
-        R[..., 0, 1] = c * s_k
-        R[..., 0, 2] = s * c
-        R[..., 0, 3] = s * s_k
-        R[..., 1, 0] = -self.k * s * c
-        R[..., 1, 1] = c**2
-        R[..., 1, 2] = -self.k * s**2
-        R[..., 1, 3] = s * c
-        R[..., 2, 0] = -s * c
-        R[..., 2, 1] = -s * s_k
-        R[..., 2, 2] = c**2
-        R[..., 2, 3] = c * s_k
-        R[..., 3, 0] = self.k * s**2
-        R[..., 3, 1] = -s * c
-        R[..., 3, 2] = -self.k * s * c
-        R[..., 3, 3] = c**2
-        R[..., 4, 5] = r56
 
         R = R.real
 
