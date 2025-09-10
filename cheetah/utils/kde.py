dc386ce6f584c2a84115713ac5fbde382d34d3fa
Jan Kaiser <jan.kaiser.email@googlemail.com>
2025-09-10 13:34:02 +0200

    Replace `pow(2)` as well

--- cheetah/utils/kde.py
+++ cheetah/utils/kde.py
@@ -62,7 +62,7 @@ def _kde_marginal_pdf(
     residuals = values - bins.repeat(*values.shape)
     kernel_values = (
         weights
-        * torch.exp(-0.5 * (residuals / sigma).pow(2))
+        * torch.exp(-0.5 * (residuals / sigma).square())
         / torch.sqrt(2 * math.pi * sigma.square())
     )
 
