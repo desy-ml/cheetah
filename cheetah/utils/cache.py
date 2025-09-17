f2d0922343d15e0d807e0051eaa9844a3221b526
Jan Kaiser <jan.kaiser.email@googlemail.com>
2025-09-17 17:07:48 +0200

    Revert "Attempt only holding on to result"
    This reverts commit e5e6a56ee06817d4891265abb406ddc77939b157.

--- cheetah/utils/cache.py
+++ cheetah/utils/cache.py
@@ -41,14 +41,10 @@ def cache_transfer_map(func):
 
         # Recompute the transfer map if the validity keys do not match
         if new_validity_key != wrapper.validity_key:
-            result = func(self, energy, species)
-
-            wrapper.cached_transfer_map = result
+            wrapper.cached_transfer_map = func(self, energy, species)
             wrapper.validity_key = new_validity_key
-        else:
-            result = wrapper.cached_transfer_map
 
-        return result
+        return wrapper.cached_transfer_map
 
     # Set initial cache state
     wrapper.cached_transfer_map = None
