a0095746f3784d046da14b25fa6f80b190fd5ed3
Ryan Roussel <rroussel@slac.stanford.edu>
2025-10-15 14:37:31 -0500

    fix line lengths

--- cheetah/accelerator/patch.py
+++ cheetah/accelerator/patch.py
@@ -12,8 +12,10 @@ generate_unique_name = UniqueNameGenerator(prefix="unnamed_element")
 
 class Patch(Element):
     """
-    Patch element that shifts the reference orbit and time. Note that this element does not
-    support batching for the offset, time offset, pitch, tilt, E_tot_offset, and E_tot_set parameters.
+    Patch element that shifts the reference orbit 
+    and time. Note that this element does not
+    support batching for the offset, time offset, 
+    pitch, tilt, E_tot_offset, and E_tot_set parameters.
 
     :param offset:  Exit face offset in (x,y,z) from Entrance in meters.
     :param time_offset: Reference time offset in seconds.
@@ -108,7 +110,8 @@ class Patch(Element):
             dim=-1,
         )
 
-        # compute the exit positions and momentum - note these computations follow bmad coordinates
+        # compute the exit positions and momentum 
+        # - note these computations follow bmad coordinates
         rotation_matrix_inv = self.rotation_matrix().inverse()
         r_vec = torch.stack(
             (
