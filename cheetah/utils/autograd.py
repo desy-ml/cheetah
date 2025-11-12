9112c93442e51c777effd575f9809676796fe21b
Christian Hespe <christian.hespe@desy.de>
2025-11-12 10:34:03 +0100

    Custom autograd for sinc expression

--- cheetah/utils/autograd.py
+++ cheetah/utils/autograd.py
@@ -3,8 +3,8 @@ import torch
 
 class Log1PDiv(torch.autograd.Function):
     """
-    Implements a custom autograd function for the compound expression log(1+x)/x.
-    The singularity at 0 is replaced by its limit.
+    Custom autograd function for the compound expression log(1+x)/x. The singularity at
+    0 is replaced by its limit.
     """
 
     # Automatically generate a custom vmap implementation
@@ -38,5 +38,51 @@ class Log1PDiv(torch.autograd.Function):
 
 
 def log1pdiv(x: torch.Tensor) -> torch.Tensor:
-    """Calculate log(1+x)/x with proper removal of its singularity."""
+    """Calculate log(1+x)/x with proper removal of its singularity at 0."""
     return Log1PDiv.apply(x)
+
+
+class Si1MDiv(torch.autograd.Function):
+    """
+    Custom autograd function for the compound expression (1-si(sqrt(x)))/x. The
+    singularity at 0 is replaced by its limit.
+    """
+
+    # Automatically generate a custom vmap implementation
+    generate_vmap_rule = True
+
+    @staticmethod
+    def setup_context(ctx, inputs, output):
+        (x,) = inputs  # inputs is always passed as a tuple
+
+        ctx.save_for_backward(x, output)
+        ctx.save_for_forward(x, output)
+
+    @staticmethod
+    def forward(x):
+        # Since x may be negative, we use complex arithmetic for the sqrt
+        sx = (torch.complex(x, x.new_zeros(())).sqrt() / torch.pi).sinc().real
+        return torch.where(x != 0, (1 - sx) / x, x.new_ones(()) / 6.0)
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        x, fx = ctx.saved_tensors
+
+        sqrt_x = torch.complex(x, x.new_zeros(())).sqrt()
+        sx = (torch.sinc(sqrt_x / torch.pi) - sqrt_x.cos()).real / (2 * x)
+
+        return grad_output * torch.where(x != 0, (sx - fx) / x, -x.new_ones(()) / 120)
+
+    @staticmethod
+    def jvp(ctx, grad_input):
+        x, fx = ctx.saved_tensors
+
+        sqrt_x = torch.complex(x, x.new_zeros(())).sqrt()
+        sx = (torch.sinc(sqrt_x / torch.pi) - sqrt_x.cos()).real / (2 * x)
+
+        return torch.where(x != 0, (sx - fx) / x, -x.new_ones(()) / 120) * grad_input
+
+
+def si1mdiv(x: torch.Tensor) -> torch.Tensor:
+    """Calculate (1 - si(sqrt(x)))/x with proper removal of its singularity at 0."""
+    return Si1MDiv.apply(x)
--- tests/test_autograd.py
+++ tests/test_autograd.py
@@ -1,12 +1,12 @@
 import torch
 
-from cheetah.utils.autograd import log1pdiv
+from cheetah.utils.autograd import log1pdiv, si1mdiv
 
 
 def test_log1plusxbyx():
     """
-    Test that verifies that the custom autograd function log1pdiv is correctly
-    implementing log(1+x)/x and its derivative, including removing the singularity at 0.
+    Verify that the custom autograd function log1pdiv is correctly implementing
+    log(1+x)/x and its derivative, including removing the singularity at 0.
     """
     test_points = torch.tensor(
         [-0.5, 0.0, 1.0], dtype=torch.float64, requires_grad=True
@@ -27,6 +27,44 @@ def test_log1plusxbyx():
     assert torch.autograd.gradcheck(
         func=log1pdiv,
         inputs=test_points,
+        check_backward_ad=True,
+        check_forward_ad=True,
+        check_batched_grad=True,
+        check_batched_forward_grad=True,
+        check_grad_dtypes=True,
+    )
+
+
+def test_siminus1xbyx():
+    """
+    Verify that the custom autograd function si1mdiv is correctly implementing
+    (1 - si(sqrt(x)))/x and its derivative, including removing the singularity at 0.
+    """
+    test_points = torch.tensor(
+        [-0.5, 0.0, 1.0], dtype=torch.float64, requires_grad=True
+    )
+    si_points = (
+        (torch.complex(test_points, test_points.new_zeros(())).sqrt() / torch.pi)
+        .sinc()
+        .real
+    )
+
+    forward = si1mdiv(test_points)
+    assert not torch.any(forward.isnan())
+    assert torch.allclose(
+        forward,
+        torch.where(
+            test_points != 0.0,
+            (1 - si_points) / test_points,
+            test_points.new_ones(()) / 6,
+        ),
+    )
+
+    # Check gradient calculation using finite difference methods
+    assert torch.autograd.gradcheck(
+        func=si1mdiv,
+        inputs=test_points,
+        rtol=0.01,
         check_backward_ad=True,
         check_forward_ad=True,
         check_batched_grad=True,
