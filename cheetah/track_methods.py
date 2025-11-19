a5ea4696b14cf2692b583f0827ab85cc2b873d57
Jan Kaiser <jan.kaiser.email@googlemail.com>
2025-11-19 13:33:23 +0100

    Add decimals to literals

--- cheetah/track_methods.py
+++ cheetah/track_methods.py
@@ -127,7 +127,7 @@ def base_ttensor(
     j1 = fx
     j2 = torch.where(
         kx2 != 0,
-        (3.0 * length - 4.0 * sx + sx * cx) / (2 * kx2.square()),
+        (3.0 * length - 4.0 * sx + sx * cx) / (2.0 * kx2.square()),
         length.pow(5) / 20.0,
     )
     j3 = torch.where(
@@ -142,7 +142,7 @@ def base_ttensor(
         / (6.0 * kx2.pow(3)),
         length.pow(7) / 56.0,
     )
-    j_denominator = kx2 - 4 * ky2
+    j_denominator = kx2 - 4.0 * ky2
     jc = torch.where(
         j_denominator != 0, (c2y - cx) / j_denominator, 0.5 * length.square()
     )
@@ -156,126 +156,134 @@ def base_ttensor(
         j_denominator != 0, (f2y - fx) / j_denominator, length.pow(5) / 120.0
     )
 
-    khk = k2 + 2 * hx * k1
+    khk = k2 + 2.0 * hx * k1
 
     vector_shape = torch.broadcast_shapes(
         length.shape, k1.shape, k2.shape, hx.shape, tilt.shape, energy.shape
     )
 
     T = torch.zeros((7, 7, 7), **factory_kwargs).repeat(*vector_shape, 1, 1, 1)
-    T[..., 0, 0, 0] = -1 / 6 * khk * (sx.square() + dx) - 0.5 * hx * kx2 * sx.square()
-    T[..., 0, 0, 1] = 2 * (-1 / 6 * khk * sx * dx + 0.5 * hx * sx * cx)
-    T[..., 0, 1, 1] = -1 / 6 * khk * dx.square() + 0.5 * hx * dx * cx
-    T[..., 0, 0, 5] = 2 * (
-        -hx / 12 / beta * khk * (3 * sx * j1 - dx.square())
+    T[..., 0, 0, 0] = (
+        -1.0 / 6.0 * khk * (sx.square() + dx) - 0.5 * hx * kx2 * sx.square()
+    )
+    T[..., 0, 0, 1] = 2.0 * (-1.0 / 6.0 * khk * sx * dx + 0.5 * hx * sx * cx)
+    T[..., 0, 1, 1] = -1.0 / 6.0 * khk * dx.square() + 0.5 * hx * dx * cx
+    T[..., 0, 0, 5] = 2.0 * (
+        -hx / 12.0 / beta * khk * (3.0 * sx * j1 - dx.square())
         + 0.5 * hx.square() / beta * sx.square()
         + 0.25 / beta * k1 * length * sx
     )
-    T[..., 0, 1, 5] = 2 * (
-        -hx / 12 / beta * khk * (sx * dx.square() - 2 * cx * j2)
+    T[..., 0, 1, 5] = 2.0 * (
+        -hx / 12.0 / beta * khk * (sx * dx.square() - 2.0 * cx * j2)
         + 0.25 * hx.square() / beta * (sx * dx + cx * j1)
         - 0.25 / beta * (sx + length * cx)
     )
     T[..., 0, 5, 5] = (
-        -(hx.square()) / 6 / beta.square() * khk * (dx.square() * dx - 2 * sx * j2)
+        -(hx.square()) / 6.0 / beta.square() * khk * (dx.square() * dx - 2.0 * sx * j2)
         + 0.5 * hx.pow(3) / beta.square() * sx * j1
         - 0.5 * hx / beta.square() * length * sx
         - 0.5 * hx / (beta.square()) * igamma2 * dx
     )
     T[..., 0, 2, 2] = k1 * k2 * jd + 0.5 * (k2 + hx * k1) * dx
-    T[..., 0, 2, 3] = 2 * (0.5 * k2 * js)
+    T[..., 0, 2, 3] = 2.0 * (0.5 * k2 * js)
     T[..., 0, 3, 3] = k2 * jd - 0.5 * hx * dx
-    T[..., 1, 0, 0] = -1 / 6 * khk * sx * (1 + 2 * cx)
-    T[..., 1, 0, 1] = 2 * (-1 / 6 * khk * dx * (1 + 2 * cx))
-    T[..., 1, 1, 1] = -1 / 3 * khk * sx * dx - 0.5 * hx * sx
-    T[..., 1, 0, 5] = 2 * (
-        -hx / 12 / beta * khk * (3 * cx * j1 + sx * dx)
+    T[..., 1, 0, 0] = -1.0 / 6.0 * khk * sx * (1.0 + 2.0 * cx)
+    T[..., 1, 0, 1] = 2.0 * (-1.0 / 6.0 * khk * dx * (1.0 + 2.0 * cx))
+    T[..., 1, 1, 1] = -1.0 / 3.0 * khk * sx * dx - 0.5 * hx * sx
+    T[..., 1, 0, 5] = 2.0 * (
+        -hx / 12.0 / beta * khk * (3.0 * cx * j1 + sx * dx)
         - 0.25 / beta * k1 * (sx - length * cx)
     )
-    T[..., 1, 1, 5] = 2 * (
-        -hx / 12 / beta * khk * (3 * sx * j1 + dx.square())
+    T[..., 1, 1, 5] = 2.0 * (
+        -hx / 12.0 / beta * khk * (3.0 * sx * j1 + dx.square())
         + 0.25 / beta * k1 * length * sx
     )
     T[..., 1, 5, 5] = (
-        -(hx.square()) / 6 / beta.square() * khk * (sx * dx.square() - 2 * cx * j2)
+        -(hx.square()) / 6.0 / beta.square() * khk * (sx * dx.square() - 2.0 * cx * j2)
         - 0.5 * hx / beta.square() * k1 * (cx * j1 - sx * dx)
         - 0.5 * hx / beta.square() * igamma2 * sx
     )
     T[..., 1, 2, 2] = k1 * k2 * js + 0.5 * (k2 + hx * k1) * sx
-    T[..., 1, 2, 3] = 2 * (0.5 * k2 * jc)
+    T[..., 1, 2, 3] = 2.0 * (0.5 * k2 * jc)
     T[..., 1, 3, 3] = k2 * js - 0.5 * hx * sx
-    T[..., 2, 0, 2] = 2 * (
-        0.5 * k2 * (cy * jc - 2 * k1 * sy * js) + 0.5 * hx * k1 * sx * sy
+    T[..., 2, 0, 2] = 2.0 * (
+        0.5 * k2 * (cy * jc - 2.0 * k1 * sy * js) + 0.5 * hx * k1 * sx * sy
     )
-    T[..., 2, 0, 3] = 2 * (0.5 * k2 * (sy * jc - 2 * cy * js) + 0.5 * hx * sx * cy)
-    T[..., 2, 1, 2] = 2 * (
-        0.5 * k2 * (cy * js - 2 * k1 * sy * jd) + 0.5 * hx * k1 * dx * sy
+    T[..., 2, 0, 3] = 2.0 * (0.5 * k2 * (sy * jc - 2.0 * cy * js) + 0.5 * hx * sx * cy)
+    T[..., 2, 1, 2] = 2.0 * (
+        0.5 * k2 * (cy * js - 2.0 * k1 * sy * jd) + 0.5 * hx * k1 * dx * sy
     )
-    T[..., 2, 1, 3] = 2 * (0.5 * k2 * (sy * js - 2 * cy * jd) + 0.5 * hx * dx * cy)
-    T[..., 2, 2, 5] = 2 * (
-        0.5 * hx / beta * k2 * (cy * jd - 2 * k1 * sy * jf)
+    T[..., 2, 1, 3] = 2.0 * (0.5 * k2 * (sy * js - 2.0 * cy * jd) + 0.5 * hx * dx * cy)
+    T[..., 2, 2, 5] = 2.0 * (
+        0.5 * hx / beta * k2 * (cy * jd - 2.0 * k1 * sy * jf)
         + 0.5 * hx.square() / beta * k1 * j1 * sy
         - 0.25 / beta * k1 * length * sy
     )
-    T[..., 2, 3, 5] = 2 * (
-        0.5 * hx / beta * k2 * (sy * jd - 2 * cy * jf)
+    T[..., 2, 3, 5] = 2.0 * (
+        0.5 * hx / beta * k2 * (sy * jd - 2.0 * cy * jf)
         + 0.5 * hx.square() / beta * j1 * cy
         - 0.25 / beta * (sy + length * cy)
     )
-    T[..., 3, 0, 2] = 2 * (
-        0.5 * k1 * k2 * (2 * cy * js - sy * jc) + 0.5 * (k2 + hx * k1) * sx * cy
+    T[..., 3, 0, 2] = 2.0 * (
+        0.5 * k1 * k2 * (2.0 * cy * js - sy * jc) + 0.5 * (k2 + hx * k1) * sx * cy
     )
-    T[..., 3, 0, 3] = 2 * (
-        0.5 * k2 * (2 * k1 * sy * js - cy * jc) + 0.5 * (k2 + hx * k1) * sx * sy
+    T[..., 3, 0, 3] = 2.0 * (
+        0.5 * k2 * (2.0 * k1 * sy * js - cy * jc) + 0.5 * (k2 + hx * k1) * sx * sy
     )
-    T[..., 3, 1, 2] = 2 * (
-        0.5 * k1 * k2 * (2 * cy * jd - sy * js) + 0.5 * (k2 + hx * k1) * dx * cy
+    T[..., 3, 1, 2] = 2.0 * (
+        0.5 * k1 * k2 * (2.0 * cy * jd - sy * js) + 0.5 * (k2 + hx * k1) * dx * cy
     )
-    T[..., 3, 1, 3] = 2 * (
-        0.5 * k2 * (2 * k1 * sy * jd - cy * js) + 0.5 * (k2 + hx * k1) * dx * sy
+    T[..., 3, 1, 3] = 2.0 * (
+        0.5 * k2 * (2.0 * k1 * sy * jd - cy * js) + 0.5 * (k2 + hx * k1) * dx * sy
     )
-    T[..., 3, 2, 5] = 2 * (
-        0.5 * hx / beta * k1 * k2 * (2 * cy * jf - sy * jd)
+    T[..., 3, 2, 5] = 2.0 * (
+        0.5 * hx / beta * k1 * k2 * (2.0 * cy * jf - sy * jd)
         + 0.5 * hx / beta * (k2 + hx * k1) * j1 * cy
         + 0.25 / beta * k1 * (sy - length * cy)
     )
-    T[..., 3, 3, 5] = 2 * (
-        0.5 * hx / beta * k2 * (2 * k1 * sy * jf - cy * jd)
+    T[..., 3, 3, 5] = 2.0 * (
+        0.5 * hx / beta * k2 * (2.0 * k1 * sy * jf - cy * jd)
         + 0.5 * hx / beta * (k2 + hx * k1) * j1 * sy
         - 0.25 / beta * k1 * length * sy
     )
-    T[..., 4, 0, 0] = -1 * (
-        hx / 12 / beta * khk * (sx * dx + 3 * j1)
+    T[..., 4, 0, 0] = -1.0 * (
+        hx / 12.0 / beta * khk * (sx * dx + 3.0 * j1)
         - 0.25 / beta * k1 * (length - sx * cx)
     )
-    T[..., 4, 0, 1] = -2 * (
-        hx / 12 / beta * khk * dx.square() + 0.25 / beta * k1 * sx.square()
+    T[..., 4, 0, 1] = -2.0 * (
+        hx / 12.0 / beta * khk * dx.square() + 0.25 / beta * k1 * sx.square()
     )
-    T[..., 4, 1, 1] = -1 * (
-        hx / 6 / beta * khk * j2 - 0.5 / beta * sx - 0.25 / beta * k1 * (j1 - sx * dx)
+    T[..., 4, 1, 1] = -1.0 * (
+        hx / 6.0 / beta * khk * j2 - 0.5 / beta * sx - 0.25 / beta * k1 * (j1 - sx * dx)
     )
-    T[..., 4, 0, 5] = -2 * (
-        hx.square() / 12 / beta.square() * khk * (3 * dx * j1 - 4 * j2)
-        + 0.25 * hx / beta.square() * k1 * j1 * (1 + cx)
+    T[..., 4, 0, 5] = -2.0 * (
+        hx.square() / 12.0 / beta.square() * khk * (3.0 * dx * j1 - 4.0 * j2)
+        + 0.25 * hx / beta.square() * k1 * j1 * (1.0 + cx)
         + 0.5 * hx / beta.square() * igamma2 * sx
     )
-    T[..., 4, 1, 5] = -2 * (
-        hx.square() / 12 / beta.square() * khk * (dx * dx.square() - 2 * sx * j2)
+    T[..., 4, 1, 5] = -2.0 * (
+        hx.square() / 12.0 / beta.square() * khk * (dx * dx.square() - 2.0 * sx * j2)
         + 0.25 * hx / beta.square() * k1 * sx * j1
         + 0.5 * hx / beta.square() * igamma2 * dx
     )
-    T[..., 4, 5, 5] = -1 * (
-        hx.pow(3) / 6 / beta.pow(3) * khk * (3 * j3 - 2 * dx * j2)
-        + hx.square() / 6 / beta.pow(3) * k1 * (sx * dx.square() - j2 * (1 + 2 * cx))
+    T[..., 4, 5, 5] = -1.0 * (
+        hx.pow(3) / 6.0 / beta.pow(3) * khk * (3.0 * j3 - 2.0 * dx * j2)
+        + hx.square()
+        / 6.0
+        / beta.pow(3)
+        * k1
+        * (sx * dx.square() - j2 * (1.0 + 2.0 * cx))
         + 1.5 / beta.pow(3) * igamma2 * (hx.square() * j1 - length)
     )
-    T[..., 4, 2, 2] = -1 * (
+    T[..., 4, 2, 2] = -1.0 * (
         -hx / beta * k1 * k2 * jf
         - 0.5 * hx / beta * (k2 + hx * k1) * j1
         + 0.25 / beta * k1 * (length - cy * sy)
     )
-    T[..., 4, 2, 3] = -2 * (-0.5 * hx / beta * k2 * jd - 0.25 / beta * k1 * sy.square())
-    T[..., 4, 3, 3] = -1 * (
+    T[..., 4, 2, 3] = -2.0 * (
+        -0.5 * hx / beta * k2 * jd - 0.25 / beta * k1 * sy.square()
+    )
+    T[..., 4, 3, 3] = -1.0 * (
         -hx / beta * k2 * jf
         + 0.5 * hx.square() / beta * j1
         - 0.25 / beta * (length + cy * sy)
