59cd84431bf71e0d2c0bc4afc5b3ab9ac51b1fa9
Jan Kaiser <jan.kaiser.email@googlemail.com>
2025-12-02 15:19:50 +0100

    Add misalignments and tilt to consistency tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -25,16 +25,19 @@ ELEMENT_SUBCLASSES_ARGS = {
         "linear": {
             "length": torch.tensor(1.0),
             "angle": torch.tensor([1.0, -2.0]),
+            "tilt": torch.tensor(0.42),
             "tracking_method": "linear",
         },
         "second_order": {
             "length": torch.tensor(1.0),
             "angle": torch.tensor([1.0, -2.0]),
+            "tilt": torch.tensor(0.42),
             "tracking_method": "second_order",
         },
         "drift_kick_drift": {
             "length": torch.tensor(1.0),
             "angle": torch.tensor([1.0, -2.0]),
+            "tilt": torch.tensor(0.42),
             "tracking_method": "drift_kick_drift",
         },
     },
@@ -57,16 +60,22 @@ ELEMENT_SUBCLASSES_ARGS = {
         "linear": {
             "length": torch.tensor(1.0),
             "k1": torch.tensor([1.0, -2.0]),
+            "tilt": torch.tensor(0.42),
+            "misalignment": torch.tensor([0.01, -0.02]),
             "tracking_method": "linear",
         },
         "second_order": {
             "length": torch.tensor(1.0),
             "k1": torch.tensor([1.0, -2.0]),
+            "tilt": torch.tensor(0.42),
+            "misalignment": torch.tensor([0.01, -0.02]),
             "tracking_method": "second_order",
         },
         "drift_kick_drift": {
             "length": torch.tensor(1.0),
             "k1": torch.tensor([1.0, -2.0]),
+            "tilt": torch.tensor(0.42),
+            "misalignment": torch.tensor([0.01, -0.02]),
             "tracking_method": "drift_kick_drift",
         },
     },
@@ -74,16 +83,19 @@ ELEMENT_SUBCLASSES_ARGS = {
         "linear": {
             "length": torch.tensor(1.0),
             "angle": torch.tensor([1.0, -2.0]),
+            "tilt": torch.tensor(0.42),
             "tracking_method": "linear",
         },
         "second_order": {
             "length": torch.tensor(1.0),
             "angle": torch.tensor([1.0, -2.0]),
+            "tilt": torch.tensor(0.42),
             "tracking_method": "second_order",
         },
         "drift_kick_drift": {
             "length": torch.tensor(1.0),
             "angle": torch.tensor([1.0, -2.0]),
+            "tilt": torch.tensor(0.42),
             "tracking_method": "drift_kick_drift",
         },
     },
@@ -95,16 +107,24 @@ ELEMENT_SUBCLASSES_ARGS = {
         "linear": {
             "length": torch.tensor(1.0),
             "k2": torch.tensor([1.0, -2.0]),
+            "tilt": torch.tensor(0.42),
+            "misalignment": torch.tensor([0.01, -0.02]),
             "tracking_method": "linear",
         },
         "second_order": {
             "length": torch.tensor(1.0),
             "k2": torch.tensor([1.0, -2.0]),
+            "tilt": torch.tensor(0.42),
+            "misalignment": torch.tensor([0.01, -0.02]),
             "tracking_method": "second_order",
         },
     },
     cheetah.Solenoid: {
-        "default": {"length": torch.tensor(1.0), "k": torch.tensor([1.0, -2.0])}
+        "default": {
+            "length": torch.tensor(1.0),
+            "k": torch.tensor([1.0, -2.0]),
+            "misalignment": torch.tensor([0.01, -0.02]),
+        }
     },
     cheetah.SpaceChargeKick: {"default": {"effect_length": torch.tensor(1.0)}},
     cheetah.TransverseDeflectingCavity: {
