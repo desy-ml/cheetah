a1eb0e2aaad39c9d4b07fd55be94c742cbd19cac
amylizzle <amylizzle@users.noreply.github.com>
2026-02-25 16:12:37 +0000

    isort

--- cheetah/__init__.py
+++ cheetah/__init__.py
@@ -1,34 +1,14 @@
 from . import converters  # noqa: F401
 from ._version import __version__  # noqa: F401
-from .accelerator import (  # noqa: F401
-    BPM,
-    Aperture,
-    Cavity,
-    CombinedCorrector,
-    CustomTransferMap,
-    Dipole,
-    Drift,
-    Element,
-    HorizontalCorrector,
-    Marker,
-    Quadrupole,
-    RBend,
-    Screen,
-    Segment,
-    Sextupole,
-    Solenoid,
-    SpaceChargeKick,
-    TransverseDeflectingCavity,
-    Undulator,
-    VerticalCorrector,
-)
+from .accelerator import (BPM, Aperture, Cavity,  # noqa: F401
+                          CombinedCorrector, CustomTransferMap, Dipole, Drift,
+                          Element, HorizontalCorrector, Marker, Quadrupole,
+                          RBend, Screen, Segment, Sextupole, Solenoid,
+                          SpaceChargeKick, TransverseDeflectingCavity,
+                          Undulator, VerticalCorrector)
 from .particles import Beam, ParameterBeam, ParticleBeam, Species  # noqa: F401
-from .utils import (  # noqa: F401
-    DefaultParameterWarning,
-    DirtyNameWarning,
-    NoBeamPropertiesInLatticeWarning,
-    NotUnderstoodPropertyWarning,
-    BadVisualizationWarning,
-    PhysicsWarning,
-    UnknownElementWarning,
-)
+from .utils import (BadVisualizationWarning,  # noqa: F401
+                    DefaultParameterWarning, DirtyNameWarning,
+                    NoBeamPropertiesInLatticeWarning,
+                    NotUnderstoodPropertyWarning, PhysicsWarning,
+                    UnknownElementWarning)
--- tests/test_3d_visualization.py
+++ tests/test_3d_visualization.py
@@ -1,7 +1,8 @@
 import numpy as np
+import pytest
 import torch
 import trimesh
-import pytest
+
 import cheetah
 
 
