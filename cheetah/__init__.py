5d0a8ed771a4be2339407eb0bd3045420c23c159
amylizzle <amylizzle@users.noreply.github.com>
2026-02-25 16:17:33 +0000

    this is madenning

--- cheetah/__init__.py
+++ cheetah/__init__.py
@@ -1,14 +1,34 @@
 from . import converters  # noqa: F401
 from ._version import __version__  # noqa: F401
-from .accelerator import (BPM, Aperture, Cavity,  # noqa: F401
-                          CombinedCorrector, CustomTransferMap, Dipole, Drift,
-                          Element, HorizontalCorrector, Marker, Quadrupole,
-                          RBend, Screen, Segment, Sextupole, Solenoid,
-                          SpaceChargeKick, TransverseDeflectingCavity,
-                          Undulator, VerticalCorrector)
+from .accelerator import Cavity  # noqa: F401
+from .accelerator import (
+    BPM,
+    Aperture,
+    CombinedCorrector,
+    CustomTransferMap,
+    Dipole,
+    Drift,
+    Element,
+    HorizontalCorrector,
+    Marker,
+    Quadrupole,
+    RBend,
+    Screen,
+    Segment,
+    Sextupole,
+    Solenoid,
+    SpaceChargeKick,
+    TransverseDeflectingCavity,
+    Undulator,
+    VerticalCorrector,
+)
 from .particles import Beam, ParameterBeam, ParticleBeam, Species  # noqa: F401
-from .utils import (BadVisualizationWarning,  # noqa: F401
-                    DefaultParameterWarning, DirtyNameWarning,
-                    NoBeamPropertiesInLatticeWarning,
-                    NotUnderstoodPropertyWarning, PhysicsWarning,
-                    UnknownElementWarning)
+from .utils import BadVisualizationWarning  # noqa: F401
+from .utils import (
+    DefaultParameterWarning,
+    DirtyNameWarning,
+    NoBeamPropertiesInLatticeWarning,
+    NotUnderstoodPropertyWarning,
+    PhysicsWarning,
+    UnknownElementWarning,
+)
