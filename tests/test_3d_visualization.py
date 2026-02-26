71044a95b175ce7a240e5fb38187765e833b93b4
Jan Kaiser <jan.kaiser.email@googlemail.com>
2026-02-26 10:48:28 +0100

    Readd new tests following test by @amylizzle

--- tests/test_3d_visualization.py
+++ tests/test_3d_visualization.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pytest
 import torch
 import trimesh
 
@@ -24,3 +25,41 @@ def test_run_and_check_return():
     assert isinstance(mesh, trimesh.Scene)
     assert isinstance(output_transform, np.ndarray)
     assert output_transform.shape == (4, 4)
+
+
+def test_no_mesh_warning():
+    """
+    Test that a warning is raised when an element's mesh cannot be loaded, and that the
+    warning message is correct.
+    """
+    element = cheetah.BPM(name="bpm1")
+
+    with pytest.warns(cheetah.VisualizationWarning) as record:
+        _, _ = element.to_mesh()
+
+        assert len(record) == 1
+        assert (
+            record[0].message.args[0]
+            == "Could not load 3D mesh for element bpm1 of type BPM. The element will "
+            "not be visualised."
+        )
+
+
+def test_zero_length_warning():
+    """
+    Test that a warning is raised when an element has a length of zero, and that the
+    warning message is correct.
+    """
+    element = cheetah.HorizontalCorrector(length=torch.tensor(0.0), name="hcorr1")
+
+    with pytest.warns(cheetah.VisualizationWarning) as record:
+        _, _ = element.to_mesh()
+
+        assert len(record) == 1
+        assert (
+            record[0].message.args[0]
+            == "Element hcorr1 of type HorizontalCorrector has a length of zero. The "
+            "mesh is therefore scaled to a default size and does not accurately "
+            "represent the element's length. If this is intentional, you can ignore "
+            "this warning."
+        )
