0ce65263c068757d4b0a7730c4d4947e0eac4eec
Jan Kaiser <jan.kaiser.email@googlemail.com>
2026-05-13 13:31:14 +0200

    Fix speed issues with tensorised cloud-in-cell implementation on CUDA

--- cheetah/utils/cloud_in_cell.py
+++ cheetah/utils/cloud_in_cell.py
@@ -1,3 +1,5 @@
+import itertools
+import math
 from typing import Sequence
 
 import torch
@@ -38,10 +40,6 @@ def cloud_in_cell_charge_deposition(
         "tensors",
     )
 
-    bin_widths = (extent[..., 1] - extent[..., 0]) / torch.tensor(
-        histogram_shape, device=positions.device
-    )
-
     # Set weights to zero for particles outside grid bounds
     inside_mask = ((extent[..., 0] <= positions) & (positions < extent[..., 1])).all(
         dim=-1
@@ -49,6 +47,8 @@ def cloud_in_cell_charge_deposition(
     masked_charges = charges * inside_mask  # Shape (..., num_samples)
 
     # Normalise particle coordinates to normalised bin space
+    bin_space_upper_bounds = torch.tensor(histogram_shape, device=positions.device) - 1
+    bin_widths = (extent[..., 1] - extent[..., 0]) / bin_space_upper_bounds
     positions_in_bin_space = (positions - extent[..., 0]) / bin_widths
     positions_in_bin_space_int_component = positions_in_bin_space.floor().long()
     positions_in_bin_space_fractional_components = (
@@ -56,25 +56,17 @@ def cloud_in_cell_charge_deposition(
     )
 
     # Generate all corner combinations and their weights
-    num_corners = 2**num_hist_dims
-    # TODO speed: torch.tensor(list(itertools.product([0, 1], repeat=D)), device=device)
-    corner_offsets = (
-        torch.arange(num_corners).unsqueeze(-1).to(positions.device)
-        // (2 ** torch.arange(num_hist_dims).to(positions.device))
-        % 2
-    )
+    corner_offsets = positions_in_bin_space_int_component.new_tensor(
+        list(itertools.product([0, 1], repeat=num_hist_dims))
+    )  # Shape (num_corners, num_hist_dims)
     corner_positions_in_bin_space = (
         positions_in_bin_space_int_component.unsqueeze(-2) + corner_offsets
-    )
-    clamped_corner_positions_in_bin_space = corner_positions_in_bin_space.clamp(
-        corner_positions_in_bin_space.new_zeros(()),
-        corner_positions_in_bin_space.new_tensor(histogram_shape) - 1,
-    )
-    corner_weight_factors = positions_in_bin_space_fractional_components.unsqueeze(
-        -2
-    ).where(
-        corner_offsets == 1,
-        1.0 - positions_in_bin_space_fractional_components.unsqueeze(-2),
+    ).clamp(bin_space_upper_bounds.new_zeros(()), bin_space_upper_bounds)
+    positions_in_bin_space_fractional_components.unsqueeze(-2)
+    corner_weight_factors = torch.where(
+        corner_offsets == 0,
+        positions_in_bin_space_fractional_components.unsqueeze(-2),
+        (1.0 - positions_in_bin_space_fractional_components).unsqueeze(-2),
     )  # Shape (..., num_samples, num_corners, num_hist_dims)
     corner_weights = corner_weight_factors.prod(
         dim=-1
@@ -84,20 +76,21 @@ def cloud_in_cell_charge_deposition(
     )  # Shape (..., num_samples, num_corners)
 
     vector_shape = positions.shape[:-2]
-    charge_grid = corner_charges.new_zeros(*vector_shape, *histogram_shape)
+    num_histogram_bins = math.prod(histogram_shape)
 
-    vector_indices = tuple(
-        torch.arange(this_vector_dim_length).reshape(
-            this_vector_dim_length, *([1] * (num_hist_dims - dim_idx + 1))
-        )
-        for dim_idx, this_vector_dim_length in enumerate(vector_shape)
+    corner_positions_in_flat_bin_space = (
+        corner_positions_in_bin_space[..., 0]
+        + histogram_shape[0] * corner_positions_in_bin_space[..., 1]
     )
 
-    charge_grid = charge_grid.index_put(
-        vector_indices + clamped_corner_positions_in_bin_space.unbind(-1),
-        corner_charges,
-        accumulate=True,
+    flat_charge_grid = corner_charges.new_zeros(*vector_shape, num_histogram_bins)
+    flat_charge_grid.scatter_add_(
+        dim=-1,
+        index=corner_positions_in_flat_bin_space.flatten(start_dim=-2),
+        src=corner_charges.flatten(start_dim=-2),
     )
+
+    charge_grid = flat_charge_grid.reshape(*vector_shape, *histogram_shape)
 
     return charge_grid
 
