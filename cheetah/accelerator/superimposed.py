76569f6420ae674451473de70568d2fde345c7c8
Jan Kaiser <jan.kaiser.email@googlemail.com>
2026-06-30 13:21:26 +0200

    Fix `Superimposed` implementation and tests

--- cheetah/accelerator/superimposed.py
+++ cheetah/accelerator/superimposed.py
@@ -26,7 +26,7 @@ class Superimposed(Element):
     def __init__(
         self,
         base_element: Element,
-        superimposed_element: Element | list[Element],
+        superimposed_element: Element,
         name: str | None = None,
         sanitize_name: bool = False,
         device: torch.device | None = None,
@@ -35,122 +35,52 @@ class Superimposed(Element):
         factory_kwargs = {"device": device, "dtype": dtype}
         super().__init__(name=name, sanitize_name=sanitize_name, **factory_kwargs)
 
-        self.base_element = base_element.to(**factory_kwargs)
+        assert superimposed_element.length == torch.tensor(
+            0.0
+        ), "The superimposed element must have zero length."
 
-        if (
-            isinstance(superimposed_element, Element)
-            and type(superimposed_element) is not Element
-        ):
-            # convert to a list of one element for uniform handling
-            self.superimposed_element = [superimposed_element.to(**factory_kwargs)]
-        elif isinstance(superimposed_element, list) and all(
-            isinstance(ele, Element) and type(ele) is not Element
-            for ele in superimposed_element
-        ):
-            superimposed_element = [
-                ele.to(**factory_kwargs) for ele in superimposed_element
-            ]
-            self.superimposed_element = superimposed_element
-        else:
-            raise TypeError(
-                "Superimposed_element must be an Element subclass"
-                " or a list of Element subclasses, got "
-                f"{superimposed_element.__class__.__name__}"
-            )
+        self.base_element = base_element
+        self.superimposed_element = superimposed_element
 
-        for superimposed_ele in self.superimposed_element:
-            if superimposed_ele.length is not None and not torch.all(
-                superimposed_ele.length == 0
-            ):
-                raise ValueError(
-                    f"Superimposed elements must have zero length, "
-                    f"but {superimposed_ele.name} has length {superimposed_ele.length}"
-                )
+        base_element_halves = base_element.split(base_element.length / 2.0)
+        self.elements = [
+            base_element_halves[0],
+            superimposed_element,
+            base_element_halves[1],
+        ]
+        self._segment = Segment(
+            elements=[
+                base_element_halves[0],
+                superimposed_element,
+                base_element_halves[1],
+            ],
+            name=f"{self.name}_segment",
+            sanitize_name=sanitize_name,
+        )
 
-        self._length = self.base_element.length.clone()
+    def flattened(self) -> "Segment":
+        return self._segment.flattened()
 
-        # Split base and insert superimposed element(s)
-        self.update_subelements()
+    @property
+    def is_skippable(self) -> bool:
+        return self._segment.is_skippable
 
     @property
     def length(self) -> torch.Tensor:
-        return self.base_element.length
-
-    def update_subelements(self):
-        """
-        Update the subelements of the superimposed element.
-
-        Called whenever base_element or superimposed_element is modified.
-        """
-        resolution = self.base_element.length.abs().max() / 2.0
-        halves = self.base_element.split(resolution=resolution)
-
-        self._segment = Segment(
-            elements=[halves[0], *self.superimposed_element, halves[1]],
-            name=f"{self.name}_segment",
-            sanitize_name=False,
-        )
-
-    @property
-    def subelements(self) -> list[Element]:
-        """
-        The two halves of the base element with the superimposed
-        element(s) in between.
-        """
-
-        if not hasattr(self, "_segment") or self._length != self.base_element.length:
-            # if base element length has been updated redraw the _segment
-            self._length = self.base_element.length.clone()
-            self.update_subelements()
-        return self._segment.elements
-
-    def track(self, incoming: Beam) -> Beam:
-        if self.is_skippable:
-            return self._track_first_order(incoming)
-        else:
-            todos = []
-            continuous_skippable_elements = []
-            for element in self.subelements:
-                if element.is_skippable:
-                    # Collect skippable elements until a non-skippable element is found
-                    continuous_skippable_elements.append(element)
-                else:
-                    # If a non-skippable element is found, merge the skippable elements
-                    # and append them before the non-skippable element
-                    if len(continuous_skippable_elements) > 0:
-                        todos.append(Segment(elements=continuous_skippable_elements))
-                        continuous_skippable_elements = []
-
-                    todos.append(element)
-
-            # If there are still skippable elements at the end of the segment append
-            # them as well
-            if len(continuous_skippable_elements) > 0:
-                todos.append(Segment(elements=continuous_skippable_elements))
-
-            for todo in todos:
-                incoming = todo.track(incoming)
-
-            return incoming
+        return self._segment.length
 
     def first_order_transfer_map(
         self, energy: torch.Tensor, species: Species
     ) -> torch.Tensor:
-        tm = torch.eye(7, device=energy.device, dtype=energy.dtype).repeat(
-            (*energy.shape, 1, 1)
-        )
-        for element in self.subelements:
-            tm = element.first_order_transfer_map(energy, species) @ tm
-        return tm
+        return self._segment.first_order_transfer_map(energy, species)
 
-    @property
-    def is_skippable(self) -> bool:
-        return all([element.is_skippable for element in self.subelements])
+    def track(self, incoming: Beam) -> Beam:
+        return self._segment.track(incoming)
 
     def plot(
         self, s: float, vector_idx: tuple | None = None, ax: plt.Axes | None = None
     ) -> plt.Axes:
-        return self.base_element.plot(s, vector_idx=vector_idx, ax=ax)
+        return self._segment.plot(s, vector_idx=vector_idx, ax=ax)
 
     @property
     def defining_features(self) -> list[str]:
--- tests/test_superimposed.py
+++ tests/test_superimposed.py
@@ -9,17 +9,19 @@ def test_superimposed_base_split_length():
     Test that the base element of a superimposed segment is correctly split into two
     halfs, each half the length of the original base element.
     """
-    superimposed_segment = cheetah.Superimposed(
+    superimposed = cheetah.Superimposed(
         base_element=cheetah.Quadrupole(length=torch.tensor(1.0)),
         superimposed_element=cheetah.BPM(),
     )
 
-    assert len(superimposed_segment.subelements) == 3
-    assert isinstance(superimposed_segment.subelements[0], cheetah.Quadrupole)
-    assert isinstance(superimposed_segment.subelements[1], cheetah.BPM)
-    assert isinstance(superimposed_segment.subelements[2], cheetah.Quadrupole)
-    assert superimposed_segment.subelements[0].length == torch.tensor(0.5)
-    assert superimposed_segment.subelements[2].length == torch.tensor(0.5)
+    assert len(superimposed._segment.elements) == 3
+    assert isinstance(superimposed._segment.elements[0], cheetah.Quadrupole)
+    assert isinstance(superimposed._segment.elements[1], cheetah.BPM)
+    assert isinstance(superimposed._segment.elements[2], cheetah.Quadrupole)
+    assert superimposed._segment.elements[0].length == torch.tensor(0.5)
+    assert superimposed._segment.elements[2].length == torch.tensor(0.5)
+
+    assert superimposed.length == torch.tensor(1.0)
 
 
 def test_superimposed_first_order_transfer_map():
@@ -28,14 +30,14 @@ def test_superimposed_first_order_transfer_map():
     first order transfer map of the base element.
     """
     quadrupole = cheetah.Quadrupole(length=torch.tensor(1.0), k1=torch.tensor(4.2))
-    superimposed_segment = cheetah.Superimposed(
+    superimposed = cheetah.Superimposed(
         base_element=quadrupole, superimposed_element=cheetah.BPM()
     )
 
     energy = torch.tensor(1.0e9)
     species = cheetah.Species("electron")
 
-    tm_superimposed = superimposed_segment.first_order_transfer_map(energy, species)
+    tm_superimposed = superimposed.first_order_transfer_map(energy, species)
     tm_quadrupole = quadrupole.first_order_transfer_map(energy, species)
 
     assert torch.allclose(tm_superimposed, tm_quadrupole)
@@ -43,8 +45,8 @@ def test_superimposed_first_order_transfer_map():
 
 def test_not_flattening():
     """
-    Test that a `Superimposed` element is not flattened when `.flattened()` is called on
-    a `Segment`, but remains as a `Superimposed` element with the correct subelements.
+    Test that a `Superimposed` element is also flattened when `.flattened()` is called
+    on a `Segment` containing it.
     """
     segment = cheetah.Segment(
         elements=[
@@ -60,9 +62,12 @@ def test_not_flattening():
     )
     flattened = segment.flattened()
 
-    assert len(flattened.elements) == 3
-    assert isinstance(flattened.elements[1], cheetah.Superimposed)
-    assert len(flattened.elements[1].subelements) == 3
+    assert len(flattened.elements) == 5
+    assert isinstance(flattened.elements[0], cheetah.Drift)
+    assert isinstance(flattened.elements[1], cheetah.Quadrupole)
+    assert isinstance(flattened.elements[2], cheetah.BPM)
+    assert isinstance(flattened.elements[3], cheetah.Quadrupole)
+    assert isinstance(flattened.elements[4], cheetah.Drift)
 
 
 def test_superimposed_element_rejects_nonzero_length():
@@ -70,7 +75,9 @@ def test_superimposed_element_rejects_nonzero_length():
     Test that an error is raised when attempting to superimpose a non-zero length
     element.
     """
-    with pytest.raises(ValueError):
+    with pytest.raises(
+        AssertionError, match="The superimposed element must have zero length."
+    ):
         _ = cheetah.Superimposed(
             base_element=cheetah.Quadrupole(length=torch.tensor(1.0)),
             superimposed_element=cheetah.Dipole(length=torch.tensor(0.5)),
