c70b02074051ba91aa3d65cedeb030e93d97ce5b
Jan Kaiser <jan.kaiser.email@googlemail.com>
2026-07-16 16:43:07 +0200

    docs: update docstrings for moved control_systems path

--- cheetah/accelerator/aperture.py
+++ cheetah/accelerator/aperture.py
@@ -31,7 +31,7 @@ class Aperture(Element):
     :param metadata: Dictionary of arbitrary, serialisable annotations attached to the
         element (e.g. control-system addresses or PVs). This information is *not* used
         in simulation and may contain any extra data the user wants to store along with
-        the lattice. See :doc:`/control_systems` for more information.
+        the lattice. See :doc:`/getting_started/control_systems` for more information.
     """
 
     def __init__(
