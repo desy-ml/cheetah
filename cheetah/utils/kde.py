c964c427976eaa825f8b147dc1bc6733c1a71528
jp-ga <97850904+jp-ga@users.noreply.github.com>
2026-07-17 13:42:15 -0700

    add stability to kde values

--- cheetah/utils/kde.py
+++ cheetah/utils/kde.py
@@ -65,6 +65,7 @@ def _kde_marginal_pdf(
         * (-0.5 * (residuals / sigma).square()).exp()
         / (2 * math.pi * sigma.square()).sqrt()
     )
+    kernel_values = kernel_values.clamp_min(torch.finfo(kernel_values.dtype).tiny)
 
     prob_mass = kernel_values.sum(dim=-2)
     normalization = prob_mass.sum(dim=-1).unsqueeze(-1) + epsilon
